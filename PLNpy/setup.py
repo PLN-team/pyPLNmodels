0a1f87be1f55ac25816c7e862e4e1a3de322206a
bastien-mva <bastien.batardiere@gmail.com>
2022-02-14 10:40:17 +0100

    reorganized the file so that it can be a package
    Former-commit-id: 544b9bf9660ba05d15946e742e3a2be890a5ffc1

---
+++ PLNpy/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from pyPLN import __version__
+
+setup(
+    name='PLNpy',
+    version='0.0.1',
+    Descrition = 'Package that implements PLN models', 
+    url='https://github.com/PLN-team/PLNpy/tree/master/fastPLNmodels',
+    author='Bastien Batardière, Julien Chiquet, Joon Kwon',
+    author_email='bastien.batardiere@gmail.com',
+
+    py_modules=['PLNpy'],
+)
