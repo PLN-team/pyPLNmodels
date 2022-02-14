e4ed3acad192b30a1c2185ad7f4d49610730187b
bastien-mva <bastien.batardiere@gmail.com>
2022-02-14 17:30:39 +0100

    reorganized the directory, added a requirements.txt
    Former-commit-id: 1f487375e6bceb15eab3d472c0d8435d407bd3fe

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from PLNpy import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setup(
+    name='PLNpy',
+    version=__version__,
+    Descrition = 'Package that implements PLN models', 
+    url='https://github.com/PLN-team/PLNpy/tree/master/PLNpy',
+    author='Bastien Batardière, Julien Chiquet, Joon Kwon',
+    author_email='bastien.batardiere@gmail.com',
+    license_files = ('LICENSE.txt',),
+    long_description=long_description,
+    packages=find_packages(),
+    python_requires='>=3.9',
+    install_requires=requirements,
+)
