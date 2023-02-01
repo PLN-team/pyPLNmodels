7c1f4f51e708e630c5da312531dc233d5b5739fc
bastien-mva <bastien.batardiere@gmail.com>
2023-02-01 10:24:56 +0100

    Reorganized the examples data

--- pyPLNmodels/test.py
+++ pyPLNmodels/test.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-Y = pd.read_csv("../example_data/Y_test")
-O = np.exp(pd.read_csv("../example_data/O_test"))
-covariates = pd.read_csv("../example_data/cov_test")
-true_Sigma = torch.from_numpy(pd.read_csv("../example_data/true_Sigma_test").values)
-true_beta = torch.from_numpy(pd.read_csv("../example_data/true_beta_test").values)
+Y = pd.read_csv("../example_data/test_data/Y_test.csv")
+O = np.exp(pd.read_csv("../example_data/test_data/O_test.csv"))
+covariates = pd.read_csv("../example_data/test_data/cov_test.csv")
+true_Sigma = torch.from_numpy(pd.read_csv("../example_data/test_data/true_parameters/true_Sigma_test.csv").values)
+true_beta = torch.from_numpy(pd.read_csv("../example_data/test_data/true_parameters/true_beta_test.csv").values)
 
 
 
