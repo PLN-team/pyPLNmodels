09e40878a7f836dd02fdbb4111cd62e8ba2b0595
bastien-mva <bastien.batardiere@gmail.com>
2023-02-01 09:58:09 +0100

    no changes.

--- pyPLNmodels/main.py
+++ pyPLNmodels/main.py
@@ -11,12 +11,12 @@ O = np.exp(pd.read_csv("../example_data/O_test"))
 covariates = pd.read_csv("../example_data/cov_test")
 true_Sigma = torch.from_numpy(pd.read_csv("../example_data/true_Sigma_test").values)
 true_beta = torch.from_numpy(pd.read_csv("../example_data/true_beta_test").values)
-n = 20
+
 
 
 pln = PLNnoPCA()
-# pln.fit(Y,O,covariates)
-# print(pln)
+pln.fit(Y,O,covariates)
+print(pln)
 
 plnpca = PLNPCA(q = 5)
 plnpca.fit(Y,O,covariates)
