2a9a71492feb16d6c5ff6be4381220da1af56269
bastien-mva <bastien.batardiere@gmail.com>
2023-02-08 09:00:59 +0100

    Changed the setup.py according to JB's advices. Not finished yet.

--- setup.py
+++ setup.py
@@ -11,15 +11,35 @@ setup(
     name='pyPLNmodels',
     version=__version__,
     description = 'Package implementing PLN models',
-    url='https://github.com/PLN-team/PLNpy/tree/master/pyPLNmodels',
+    url=,
+    project_urls={
+        "Source": 'https://github.com/PLN-team/PLNpy/tree/master/pyPLNmodels', 
+        }
     author='Bastien Batardière, Julien Chiquet, Joon Kwon',
-    author_email='bastien.batardiere@gmail.com',
+    author_email='bastien.batardiere@gmail.com, julien.chiquet@inrae.fr, joon.kwon@inrae.fr',
     license_files = ('LICENSE.txt',),
     long_description=long_description,
     packages=find_packages(),
-    python_requires='>=3.0',
+    python_requires='>=3',
     keywords=['python','count', 'data', 'count data', 'high dimension', 'scRNAseq', 'PLN'],
     install_requires=requirements,
     py_modules=['pyPLNmodels._utils','pyPLNmodels.elbos','pyPLNmodels.VEM','pyPLNmodels._closed_forms'],
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    license="MIT",
+         # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Science/Research",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3 :: Only",
+        ],
+
 )
