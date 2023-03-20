63d8c40beba6b114cc9e0faae70a44671910c7a9
bastien-mva <bastien.batardiere@gmail.com>
2023-03-20 15:28:03 +0100

    simple test.py file to try.

--- test.py
+++ test.py
@@ -8,22 +8,15 @@ if torch.cuda.is_available():
 else:
     DEVICE = "cpu"
 
-
 Y = pd.read_csv("./example_data/real_data/oaks_counts.csv")
 covariates = None
 O = np.log(pd.read_csv("./example_data/real_data/oaks_offsets.csv"))
 
-# pln = PLN()
-# pln.fit(Y,covariates,O)
-# pln.save_model("param")
-# pln_bis = PLN()
-# pln_bis.load_model_from_file("param")
-# print(pln_bis.beta)
-
+pln = PLN()
+pln.fit(Y,covariates,O)
+print(pln)
 
 pca = PLNPCA(ranks=[4, 5])
 pca.fit(Y, covariates, O, tol=0.1)
 print(pca.best_model())
-# pca.save_model(5,"param")
-# pca_bis = PLNPCA(ranks = [4,5])
-# pca_bis.load_model_from_file(5,"param")
+
