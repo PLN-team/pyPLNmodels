6f98eec9611d272a4bed10c215284ab3e2fe7333
bastien-mva <bastien.batardiere@gmail.com>
2023-03-22 18:50:17 +0100

    Chaged version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = "0.0.19"
+VERSION = "0.0.20"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
