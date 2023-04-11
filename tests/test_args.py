f37a164727d3390cab2a88f032e6d0a58b3785fe
bastien-mva <bastien.batardiere@gmail.com>
2023-04-11 09:17:40 +0200

    import required dependencies, updated the sampling data and add tests

--- tests/test_args.py
+++ tests/test_args.py
@@ -1,13 +1,18 @@
 from pyPLNmodels.VEM import PLN, PLNPCA
+from pyPLNmodels import get_simulated_count_data
 import pytest
-from pytest_lazyfixture import lazy_fixture
+from pytest_lazyfixture import lazy_fixture as lf
+import pandas as pd
 import numpy as np
-from tests.utils import get_simulated_data, get_real_data, MSE
 
-Y_sim, covariates_sim, O_sim, true_Sigma, true_beta = get_simulated_data()
+(
+    counts_sim,
+    covariates_sim,
+    offsets_sim,
+) = get_simulated_count_data()
+
 
 RANKS = [4, 8]
-print("ca marche")
 
 
 @pytest.fixture
@@ -17,4 +22,12 @@ def my_instance_plnpca():
 
 
 def test_pandas_init(my_instance_plnpca):
-    my_instance_plnpca.fit(Y_sim, covariates_sim, O_sim)
+    my_instance_plnpca.fit(
+        pd.DataFrame(counts_sim.numpy()),
+        pd.DataFrame(covariates_sim.numpy()),
+        pd.DataFrame(offsets_sim.numpy()),
+    )
+
+
+def test_best_model(best_models):
+    print(best_models)
