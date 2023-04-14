09cce66a74fbf02fae0f2c8deb9bd554e7b43811
bastien-mva <bastien.batardiere@gmail.com>
2023-04-14 14:37:19 +0200

    simple file that tries the noS plnpca.

--- test.py
+++ test.py
@@ -10,7 +10,22 @@ print(os.getcwd())
 os.chdir("pyPLNmodels/")
 
 Y = get_real_count_data()
+q = 5
+lr = 0.01
+# nb_max = 1000
+pca = _PLNPCA(q)
+pca.fit(Y, tol=0.00001, lr=lr)
+pca.show()
 
-pca = _PLNPCA(3)
-
-pca.fit(Y)
+# pca = _PLNPCA(q)
+# pca.fit(Y, tol = 0.000000001)
+# pca.show()
+nospca = _PLNPCA_noS(q)
+nospca.fit(Y, tol=0.00001, lr=lr)
+nospca.show()
+abscisse_pca = np.arange(len(pca.elbos_list))
+abscisse_nospca = np.arange(len(nospca.elbos_list))
+plt.plot(abscisse_pca, pca.elbos_list, label="pca")
+plt.plot(abscisse_nospca, nospca.elbos_list, label="nospca")
+plt.legend()
+plt.show()
