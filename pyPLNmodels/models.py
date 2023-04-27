correcting posterior estimator of covariance for PLNPCA

pyPLNmodels/models.py
@@ -877,7 +877,12 @@ class _PLNPCA(_PLN):
     @property
     def covariance(self):
         if hasattr(self, "_components"):
-            return torch.matmul(self._components, self._components.T).detach()
+            cov_latent = self._latent_mean.T @ self._latent_mean
+            cov_latent += torch.diag(
+                torch.sum(self._latent_var * self._latent_var), dim=0)
+            )
+            cov_latent /= self.n_samples
+            return (self._components @ cov_latent @ self._components.T).detach()
         return None
 
     @property