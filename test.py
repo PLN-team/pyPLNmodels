87d2dbea2e35de84b19470851bb43221c9e248a3
bastien-mva <bastien.batardiere@gmail.com>
2023-04-28 16:02:37 +0200

    minor changes in the test file.

--- test.py
+++ test.py
@@ -13,13 +13,12 @@ covariates = None
 offsets = None
 # counts, covariates, offsets = get_simulated_count_data(seed = 0)
 
-# pca = PLNPCA(counts, covariates, offsets, ranks=[3, 4])
-# pca.fit(tol = 0.1)
+pca = PLNPCA(counts, covariates, offsets, ranks=[3, 4])
+pca.fit(tol=0.1)
 
 # pca.fit()
 # print(pca)
-data = pd.DataFrame(counts)
-print("data :", data)
+# data = pd.DataFrame(counts)
 # pln = PLN("counts~1", data)
 # pln.fit()
 # print(pln)
