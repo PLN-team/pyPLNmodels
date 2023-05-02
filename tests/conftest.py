150874a8485e2c00e7bf3707efcdbefea8cc369e
bastien-mva <bastien.batardiere@gmail.com>
2023-05-02 18:49:50 +0200

    Tried to implement all the fixtures in once. Getting trouble with pytest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,399 @@
 import sys
+import glob
+from functools import singledispatch
+
+import pytest
+from pytest_lazyfixture import lazy_fixture as lf
+from pyPLNmodels import load_model, load_plnpca
+from tests.import_fixtures import get_dict_fixtures
+from pyPLNmodels.models import PLN, _PLNPCA, PLNPCA
 
 sys.path.append("../")
+
+
+pln_full_fixture = get_dict_fixtures(PLN)
+plnpca_fixture = get_dict_fixtures(_PLNPCA)
+
+
+from tests.import_data import (
+    data_sim_0cov,
+    data_sim_2cov,
+    data_real,
+)
+
+
+counts_sim_0cov = data_sim_0cov["counts"]
+covariates_sim_0cov = data_sim_0cov["covariates"]
+offsets_sim_0cov = data_sim_0cov["offsets"]
+
+counts_sim_2cov = data_sim_2cov["counts"]
+covariates_sim_2cov = data_sim_2cov["covariates"]
+offsets_sim_2cov = data_sim_2cov["offsets"]
+
+counts_real = data_real["counts"]
+
+
+def add_fixture_to_dict(my_dict, string_fixture):
+    my_dict[string_fixture] = lf(string_fixture)
+    return my_dict
+
+
+def add_list_of_fixture_to_dict(
+    my_dict, name_of_list_of_fixtures, list_of_string_fixtures
+):
+    my_dict[name_of_list_of_fixtures] = []
+    for string_fixture in list_of_string_fixtures:
+        my_dict[name_of_list_of_fixtures].append(lf(string_fixture))
+    return my_dict
+
+
+RANK = 8
+RANKS = [2, 6]
+
+# dict_fixtures_models = []
+
+
+@singledispatch
+def convenient_plnpca(
+    counts,
+    covariates=None,
+    offsets=None,
+    offsets_formula=None,
+    dict_initialization=None,
+):
+    return _PLNPCA(
+        counts, covariates, offsets, rank=RANK, dict_initialization=dict_initialization
+    )
+
+
+@convenient_plnpca.register(str)
+def _(formula, data, offsets_formula, dict_initialization=None):
+    return _PLNPCA(formula, data, rank=RANK, dict_initialization=dict_initialization)
+
+
+@singledispatch
+def convenientplnpca(
+    counts,
+    covariates=None,
+    offsets=None,
+    offsets_formula=None,
+    dict_initialization=None,
+):
+    return PLNPCA(
+        counts,
+        covariates,
+        offsets,
+        offsets_formula,
+        dict_of_dict_initialization=dict_initialization,
+        ranks=RANKS,
+    )
+
+
+@convenientplnpca.register(str)
+def _(formula, data, offsets_formula, dict_initialization=None):
+    return PLNPCA(
+        formula,
+        data,
+        offsets_formula,
+        ranks=RANKS,
+        dict_of_dict_initialization=dict_initialization,
+    )
+
+
+params = [PLN, convenient_plnpca, convenientplnpca]
+dict_fixtures = {}
+
+
+@pytest.fixture(params=params)
+def simulated_pln_0cov_array(request):
+    cls = request.param
+    pln_full = cls(counts_sim_0cov, covariates_sim_0cov, offsets_sim_0cov)
+    return pln_full
+
+
+@pytest.fixture
+def simulated_fitted_pln_0cov_array(simulated_pln_0cov_array):
+    simulated_pln_0cov_array.fit()
+    return simulated_pln_0cov_array
+
+
+@pytest.fixture(params=params)
+def simulated_pln_0cov_formula(request):
+    cls = request.param
+    pln_full = cls("counts ~ 0", data_sim_0cov)
+    return pln_full
+
+
+@pytest.fixture
+def simulated_fitted_pln_0cov_formula(simulated_pln_0cov_formula):
+    simulated_pln_0cov_formula.fit()
+    return simulated_pln_0cov_formula
+
+
+@pytest.fixture
+def simulated_loaded_pln_0cov_formula(simulated_fitted_pln_0cov_formula):
+    simulated_fitted_pln_0cov_formula.save()
+    path = simulated_fitted_pln_0cov_formula.model_path
+    name = simulated_fitted_pln_0cov_formula.NAME
+    if name == "PLN" or name == "_PLNPCA":
+        init = load_model(path)
+    if name == "PLNPCA":
+        init = load_plnpca(path)
+    new = simulated_loaded_pln_0cov_formula.get_class(
+        "counts ~0", data_sim_0cov, dict_initialization=init
+    )
+    return new
+
+
+@pytest.fixture
+def simulated_loaded_pln_0cov_array(simulated_fitted_pln_0cov_array):
+    simulated_fitted_pln_0cov_array.save()
+    path = simulated_fitted_pln_0cov_array.model_path
+    name = simulated_fitted_pln_0cov_array.NAME
+    if name == "PLN" or name == "_PLNPCA":
+        init = load_model(path)
+    if name == "PLNPCA":
+        init = load_plnpca(path)
+    new = simulated_fitted_pln_0cov_array.get_class(
+        counts_sim_0cov,
+        covariates_sim_0cov,
+        offsets_sim_0cov,
+        dict_initialization=init,
+    )
+    return new
+
+
+sim_pln_0cov_instance = [
+    "simulated_pln_0cov_array",
+    "simulated_pln_0cov_formula",
+]
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "sim_pln_0cov_instance", sim_pln_0cov_instance
+)
+
+sim_pln_0cov_fitted = [
+    "simulated_fitted_pln_0cov_array",
+    "simulated_fitted_pln_0cov_formula",
+]
+
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "sim_pln_0cov_fitted", sim_pln_0cov_fitted
+)
+
+sim_pln_0cov_loaded = [
+    "simulated_loaded_pln_0cov_array",
+    "simulated_loaded_pln_0cov_formula",
+]
+
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "sim_pln_0cov_loaded", sim_pln_0cov_loaded
+)
+
+sim_pln_0cov = sim_pln_0cov_instance + sim_pln_0cov_fitted + sim_pln_0cov_loaded
+dict_fixtures = add_list_of_fixture_to_dict(dict_fixtures, "sim_pln_0cov", sim_pln_0cov)
+
+
+@pytest.fixture(params=params)
+def simulated_pln_2cov_array(request):
+    cls = request.param
+    pln_full = cls(counts_sim_2cov, covariates_sim_2cov, offsets_sim_2cov)
+    return pln_full
+
+
+@pytest.fixture
+def simulated_fitted_pln_2cov_array(simulated_pln_2cov_array):
+    simulated_pln_2cov_array.fit()
+    return simulated_pln_2cov_array
+
+
+@pytest.fixture(params=params)
+def simulated_pln_2cov_formula():
+    pln_full = cls("counts ~ 0 + covariates", data_sim_2cov)
+    return pln_full
+
+
+@pytest.fixture
+def simulated_fitted_pln_2cov_formula(simulated_pln_2cov_formula):
+    simulated_pln_2cov_formula.fit()
+    return simulated_pln_2cov_formula
+
+
+@pytest.fixture
+def simulated_loaded_pln_2cov_formula(simulated_fitted_pln_2cov_formula):
+    simulated_fitted_pln_2cov_formula.save()
+    path = simulated_fitted_pln_2cov_formula.model_path
+    name = simulated_fitted_pln_2cov_formula.NAME
+    if name == "PLN":
+        init = load_model(path)
+    if name == "PLNPCA":
+        init = load_plnpca(path)
+    new = simulated_fitted_pln_2cov_formula.get_class(
+        "counts ~1", data_sim_2cov, dict_initialization=init
+    )
+    return new
+
+
+@pytest.fixture
+def simulated_loaded_pln_2cov_array(simulated_fitted_pln_2cov_array):
+    simulated_fitted_pln_2cov_array.save()
+    path = simulated_fitted_pln_2cov_array.model_path
+    name = simulated_fitted_pln_2cov_array.NAME
+    if name == "PLN" or name == "_PLNPCA":
+        init = load_model(path)
+    if name == "PLNPCA":
+        init = load_model(path)
+    new = simulated_fitted_pln_2cov_array.get_class(
+        counts_sim_2cov,
+        covariates_sim_2cov,
+        offsets_sim_2cov,
+        dict_initialization=init,
+    )
+    return new
+
+
+sim_pln_2cov_instance = [
+    "simulated_pln_2cov_array",
+    "simulated_pln_2cov_formula",
+]
+
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "sim_pln_2cov_instance", sim_pln_2cov_instance
+)
+
+sim_pln_2cov_fitted = [
+    "simulated_fitted_pln_2cov_array",
+    "simulated_fitted_pln_2cov_formula",
+]
+
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "sim_pln_2cov_fitted", sim_pln_2cov_fitted
+)
+
+sim_pln_2cov_loaded = [
+    "simulated_loaded_pln_2cov_array",
+    "simulated_loaded_pln_2cov_formula",
+]
+
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "sim_pln_2cov_loaded", sim_pln_2cov_loaded
+)
+
+sim_pln_2cov = sim_pln_2cov_instance + sim_pln_2cov_fitted + sim_pln_2cov_loaded
+dict_fixtures = add_list_of_fixture_to_dict(dict_fixtures, "sim_pln_2cov", sim_pln_2cov)
+
+
+@pytest.fixture(params=params)
+def real_pln_intercept_array(request):
+    cls = request.param
+    pln_full = cls(counts_real)
+    return pln_full
+
+
+@pytest.fixture
+def real_fitted_pln_intercept_array(real_pln_intercept_array):
+    real_pln_intercept_array.fit()
+    return real_pln_intercept_array
+
+
+@pytest.fixture(params=params)
+def real_pln_intercept_formula(request):
+    cls = request.param
+    pln_full = cls("counts ~ 1", data_real)
+    return pln_full
+
+
+@pytest.fixture
+def real_fitted_pln_intercept_formula(real_pln_intercept_formula):
+    real_pln_intercept_formula.fit()
+    return real_pln_intercept_formula
+
+
+@pytest.fixture
+def real_loaded_pln_intercept_formula(real_fitted_pln_intercept_formula):
+    real_fitted_pln_intercept_formula.save()
+    path = real_fitted_pln_intercept_formula.model_path
+    name = real_fitted_pln_intercept_formula.NAME
+    if name == "PLN" or name == "_PLNPCA":
+        init = load_model(path)
+    if name == "PLNPCA":
+        init = load_plnpca(path)
+    new = real_fitted_pln_intercept_formula.get_class(
+        "counts~ 1", data_real, dict_initialization=init
+    )
+    return new
+
+
+@pytest.fixture
+def real_loaded_pln_intercept_array(real_fitted_pln_intercept_array):
+    real_fitted_pln_intercept_array.save()
+    path = real_fitted_pln_intercept_array.model_path
+    name = real_fitted_pln_intercept_array.NAME
+    if name == "PLN" or name == "_PLNPCA":
+        init = load_model(path)
+    if name == "PLNPCA":
+        init = load_plnpca(path)
+    new = real_fitted_pln_intercept_array.get_class(
+        counts_real, dict_initialization=init
+    )
+    return new
+
+
+real_pln_instance = [
+    "real_pln_intercept_array",
+    "real_pln_intercept_formula",
+]
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "real_pln_instance", real_pln_instance
+)
+
+real_pln_fitted = [
+    "real_fitted_pln_intercept_array",
+    "real_fitted_pln_intercept_formula",
+]
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "real_pln_fitted", real_pln_fitted
+)
+
+real_pln_loaded = [
+    "real_loaded_pln_intercept_array",
+    "real_loaded_pln_intercept_formula",
+]
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "real_pln_loaded", real_pln_loaded
+)
+
+sim_loaded_pln = sim_pln_0cov_loaded + sim_pln_2cov_loaded
+
+loaded_pln = real_pln_loaded + sim_loaded_pln
+dict_fixtures = add_list_of_fixture_to_dict(dict_fixtures, "loaded_pln", loaded_pln)
+
+simulated_pln_fitted = sim_pln_0cov_fitted + sim_pln_2cov_fitted
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "simulated_pln_fitted", simulated_pln_fitted
+)
+
+fitted_pln = real_pln_fitted + simulated_pln_fitted
+dict_fixtures = add_list_of_fixture_to_dict(dict_fixtures, "fitted_pln", fitted_pln)
+
+loaded_and_fitted_pln = fitted_pln + loaded_pln
+dict_fixtures = add_list_of_fixture_to_dict(
+    dict_fixtures, "loaded_and_fitted_pln", loaded_and_fitted_pln
+)
+
+real_pln = real_pln_instance + real_pln_fitted + real_pln_loaded
+dict_fixtures = add_list_of_fixture_to_dict(dict_fixtures, "real_pln", real_pln)
+
+sim_pln = sim_pln_2cov + sim_pln_0cov
+dict_fixtures = add_list_of_fixture_to_dict(dict_fixtures, "sim_pln", sim_pln)
+
+all_pln = real_pln + sim_pln
+dict_fixtures = add_list_of_fixture_to_dict(dict_fixtures, "all_pln", all_pln)
+
+
+for string_fixture in all_pln:
+    dict_fixtures = add_fixture_to_dict(dict_fixtures, string_fixture)
+
+pytest_plugins = [
+    fixture_file.replace("/", ".").replace(".py", "")
+    for fixture_file in glob.glob("src/**/tests/fixtures/[!__]*.py", recursive=True)
+]
--- tests/test_common.py
+++ tests/test_common.py
@@ -1,191 +1,51 @@
+import os
+
 import torch
 import numpy as np
 import pandas as pd
-
-from pyPLNmodels.models import PLN, _PLNPCA
-from pyPLNmodels import get_simulated_count_data, get_real_count_data
-from tests.utils import MSE
-
 import pytest
 from pytest_lazyfixture import lazy_fixture as lf
-import os
 
-(
-    counts_sim,
-    covariates_sim,
-    offsets_sim,
-    true_covariance,
-    true_coef,
-) = get_simulated_count_data(return_true_param=True, nb_cov=2)
+from pyPLNmodels.models import PLN, _PLNPCA
+from tests.utils import MSE
+
+# from tests.import_fixtures import get_dict_fixtures
+
+from tests.conftest import dict_fixtures
 
 
-counts_real = get_real_count_data()
-rank = 8
+# dict_fixtures_pln_full = dict_fixtures_models[0]
+# dict_fixtures_plnpca = dict_fixtures_models[1]
+# dict_fixturesplnpca = dict_fixtures_models[2]
 
 
-@pytest.fixture
-def instance_pln_full():
-    pln_full = PLN()
-    return pln_full
+def get_pln_and_plncpca_fixtures(key):
+    return dict_fixtures_pln_full[key] + dict_fixtures_plnpca[key]
 
 
-@pytest.fixture
-def instance__plnpca():
-    plnpca = _PLNPCA(rank=rank)
-    return plnpca
+def get_pca_fixtures(key):
+    return dict_fixtures_plnpca[key] + dict_fixturesplnpca[key]
 
 
-@pytest.fixture
-def simulated_fitted_pln_full():
-    pln_full = PLN()
-    pln_full.fit(counts=counts_sim, covariates=covariates_sim, offsets=offsets_sim)
-    return pln_full
-
-
-@pytest.fixture
-def simulated_fitted__plnpca():
-    plnpca = _PLNPCA(rank=rank)
-    plnpca.fit(counts=counts_sim, covariates=covariates_sim, offsets=offsets_sim)
-    return plnpca
-
-
-@pytest.fixture
-def loaded_simulated_pln_full(simulated_fitted_pln_full):
-    simulated_fitted_pln_full.save()
-    loaded_pln_full = PLN()
-    loaded_pln_full.load()
-    return loaded_pln_full
-
-
-@pytest.fixture
-def loaded_refit_simulated_pln_full(loaded_simulated_pln_full):
-    loaded_simulated_pln_full.fit(
-        counts=counts_sim,
-        covariates=covariates_sim,
-        offsets=offsets_sim,
-        keep_going=True,
-    )
-    return loaded_simulated_pln_full
-
-
-@pytest.fixture
-def loaded_simulated__plnpca(simulated_fitted__plnpca):
-    simulated_fitted__plnpca.save()
-    loaded_pln_full = _PLNPCA(rank=rank)
-    loaded_pln_full.load()
-    return loaded_pln_full
-
-
-@pytest.fixture
-def loaded_refit_simulated__plnpca(loaded_simulated__plnpca):
-    loaded_simulated__plnpca.fit(
-        counts=counts_sim,
-        covariates=covariates_sim,
-        offsets=offsets_sim,
-        keep_going=True,
-    )
-    return loaded_simulated__plnpca
-
-
-@pytest.fixture
-def real_fitted_pln_full():
-    pln_full = PLN()
-    pln_full.fit(counts=counts_real)
-    return pln_full
-
-
-@pytest.fixture
-def loaded_real_pln_full(real_fitted_pln_full):
-    real_fitted_pln_full.save()
-    loaded_pln_full = PLN()
-    loaded_pln_full.load()
-    return loaded_pln_full
-
-
-@pytest.fixture
-def loaded_refit_real_pln_full(loaded_real_pln_full):
-    loaded_real_pln_full.fit(counts=counts_real, keep_going=True)
-    return loaded_real_pln_full
-
-
-@pytest.fixture
-def real_fitted__plnpca():
-    plnpca = _PLNPCA(rank=rank)
-    plnpca.fit(counts=counts_real)
-    return plnpca
-
-
-@pytest.fixture
-def loaded_real__plnpca(real_fitted__plnpca):
-    real_fitted__plnpca.save()
-    loaded_plnpca = _PLNPCA(rank=rank)
-    loaded_plnpca.load()
-    return loaded_plnpca
-
-
-@pytest.fixture
-def loaded_refit_real__plnpca(loaded_real__plnpca):
-    loaded_real__plnpca.fit(counts=counts_real, keep_going=True)
-    return loaded_real__plnpca
-
-
-real_pln_full = [
-    lf("real_fitted_pln_full"),
-    lf("loaded_real_pln_full"),
-    lf("loaded_refit_real_pln_full"),
-]
-real__plnpca = [
-    lf("real_fitted__plnpca"),
-    lf("loaded_real__plnpca"),
-    lf("loaded_refit_real__plnpca"),
-]
-simulated_pln_full = [
-    lf("simulated_fitted_pln_full"),
-    lf("loaded_simulated_pln_full"),
-    lf("loaded_refit_simulated_pln_full"),
-]
-simulated__plnpca = [
-    lf("simulated_fitted__plnpca"),
-    lf("loaded_simulated__plnpca"),
-    lf("loaded_refit_simulated__plnpca"),
-]
-
-loaded_sim_pln = [
-    lf("loaded_simulated__plnpca"),
-    lf("loaded_simulated_pln_full"),
-    lf("loaded_refit_simulated_pln_full"),
-    lf("loaded_refit_simulated_pln_full"),
-]
-
-
-@pytest.mark.parametrize("loaded", loaded_sim_pln)
-def test_refit_not_keep_going(loaded):
-    loaded.fit(
-        counts=counts_sim,
-        covariates=covariates_sim,
-        offsets=offsets_sim,
-        keep_going=False,
+def get_all_fixtures(key):
+    return (
+        dict_fixtures_plnpca[key]
+        + dict_fixtures_pln_full[key]
+        + dict_fixturesplnpca[key]
     )
 
 
-all_instances = [lf("instance__plnpca"), lf("instance_pln_full")]
-
-all_fitted__plnpca = simulated__plnpca + real__plnpca
-all_fitted_pln_full = simulated_pln_full + real_pln_full
-
-simulated_any_pln = simulated__plnpca + simulated_pln_full
-real_any_pln = real_pln_full + real__plnpca
-all_fitted_models = simulated_any_pln + real_any_pln
-
-
-@pytest.mark.parametrize("any_pln", all_fitted_models)
+# @pytest.mark.parametrize("any_pln", [dict_fixtures["simulated_pln_0cov_array"]])
+@pytest.mark.parametrize("any_pln", dict_fixtures["loaded_and_fitted_pln"])
 def test_properties(any_pln):
-    assert hasattr(any_pln, "latent_variables")
-    assert hasattr(any_pln, "model_parameters")
-    assert hasattr(any_pln, "latent_parameters")
-    assert hasattr(any_pln, "optim_parameters")
+    if any_pln.NAME in ("PLN", "_PLNPCA"):
+        assert hasattr(any_pln, "latent_variables")
+        assert hasattr(any_pln, "model_parameters")
+        assert hasattr(any_pln, "latent_parameters")
+        assert hasattr(any_pln, "optim_parameters")
 
 
+"""
 @pytest.mark.parametrize("any_pln", all_fitted_models)
 def test_show_coef_transform_covariance_pcaprojected(any_pln):
     any_pln.show()
@@ -257,8 +117,8 @@ def test_find_right_coef(sim_pln):
     assert mse_coef < 0.1
 
 
-def test_number_of_iterations_pln_full(simulated_fitted_pln_full):
-    nb_iterations = len(simulated_fitted_pln_full.elbos_list)
+def test_number_of_iterations_pln_full(simulated_fitted_pln_full_0cov):
+    nb_iterations = len(simulated_fitted_pln_full_0cov.elbos_list)
     assert 50 < nb_iterations < 300
 
 
@@ -273,21 +133,21 @@ def test_computable_elbopca(instance__plnpca, simulated_fitted__plnpca):
     instance__plnpca.compute_elbo()
 
 
-def test_computable_elbo_full(instance_pln_full, simulated_fitted_pln_full):
-    instance_pln_full.counts = simulated_fitted_pln_full.counts
-    instance_pln_full.covariates = simulated_fitted_pln_full.covariates
-    instance_pln_full.offsets = simulated_fitted_pln_full.offsets
-    instance_pln_full.latent_mean = simulated_fitted_pln_full.latent_mean
-    instance_pln_full.latent_var = simulated_fitted_pln_full.latent_var
-    instance_pln_full.covariance = simulated_fitted_pln_full.covariance
-    instance_pln_full.coef = simulated_fitted_pln_full.coef
+def test_computable_elbo_full(instance_pln_full, simulated_fitted_pln_full_0cov):
+    instance_pln_full.counts = simulated_fitted_pln_full_0cov.counts
+    instance_pln_full.covariates = simulated_fitted_pln_full_0cov.covariates
+    instance_pln_full.offsets = simulated_fitted_pln_full_0cov.offsets
+    instance_pln_full.latent_mean = simulated_fitted_pln_full_0cov.latent_mean
+    instance_pln_full.latent_var = simulated_fitted_pln_full_0cov.latent_var
+    instance_pln_full.covariance = simulated_fitted_pln_full_0cov.covariance
+    instance_pln_full.coef = simulated_fitted_pln_full_0cov.coef
     instance_pln_full.compute_elbo()
 
 
-def test_fail_count_setter(simulated_fitted_pln_full):
+def test_fail_count_setter(simulated_fitted_pln_full_0cov):
     wrong_counts = torch.randint(size=(10, 5), low=0, high=10)
     with pytest.raises(Exception):
-        simulated_fitted_pln_full.counts = wrong_counts
+        simulated_fitted_pln_full_0cov.counts = wrong_counts
 
 
 @pytest.mark.parametrize("any_pln", all_fitted_models)
@@ -334,3 +194,6 @@ def test_wrong_rank():
     instance = _PLNPCA(counts_sim.shape[1] + 1)
     with pytest.warns(UserWarning):
         instance.fit(counts=counts_sim, covariates=covariates_sim, offsets=offsets_sim)
+
+
+"""
