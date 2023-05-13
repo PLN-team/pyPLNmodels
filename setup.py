396b7e0c58f5826e13a1d433fc57dc9d7bce6fb8
bastien-mva <bastien.batardiere@gmail.com>
2023-05-13 12:11:36 +0200

    try to upload to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = "0.0.38"
+VERSION = "0.0.41"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
