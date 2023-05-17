241b4dc002e0c3829d044c8d0d7da2bda1a9818b
bastien-mva <bastien.batardiere@gmail.com>
2023-05-17 16:36:47 +0200

    change version to 0.0.47

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = "0.0.43"
+VERSION = "0.0.47"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
