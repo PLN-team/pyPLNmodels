71ce0d37f328cd9a03677eab0258526b67c2fded
bastien-mva <bastien.batardiere@gmail.com>
2023-07-19 11:36:49 +0200

    changed little things

--- pca.py
+++ pca.py
@@ -4,16 +4,19 @@ from benchmark import get_sc_mark_data
 from sklearn.decomposition import PCA
 import seaborn as sns
 import numpy as np
+import time
 
 fig, axes = plt.subplots(1, 2, figsize=(20, 10))
 
-n = 1000
-p = 14059
+n = 100
+p = 14000
 Y, _, GT = get_sc_mark_data(max_n=n, dim=p)
 
 
-plnpca = PlnPCA(Y, rank=50)
-plnpca.fit(tol=0.05)
+plnpca = PlnPCA(Y, rank=80)
+t = time.time()
+plnpca.fit(tol=0.05, verbose=True)
+print("time took:", (time.time() - t) / 60)
 
 
 plnpca.viz(colors=GT, ax=axes[0])
@@ -28,4 +31,4 @@ axes[1].set_xlabel("PC1")
 axes[1].set_ylabel("PC2")
 axes[1].set_title("Standard Principal Component Analysis")
 plt.savefig("paper/plnpca_vs_pca.png", format="png")
-plt.show()
+# plt.show()
