f544e746308e627c550fea6fa32237732e7a3080
bastien-mva <bastien.batardiere@gmail.com>
2023-07-20 17:34:53 +0200

    written the optimzer by setting only "Adam" or "Rprop"

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import torch
 
 # counts = get_real_count_data()
 
-counts, covariates, offsets = get_simulated_count_data(n_samples=20000)
+counts, covariates, offsets = get_simulated_count_data(n_samples=800)
 # pca = Pln(counts,exog = covariates, offsets = offsets, batch_size = 20)
 # pca = PlnPCA(counts, batch_size = None, rank=15)
 # pca.fit(nb_max_iteration=8000, class_optimizer=torch.optim.Rprop)
-pca = PlnPCA(counts, batch_size=None, rank=15)
-pca.fit(nb_max_iteration=20, class_optimizer=torch.optim.Rprop, verbose=True)
+pca = PlnPCA(counts, batch_size=15, rank=15)
+pca.fit(nb_max_iteration=200, class_optimizer="Adam", verbose=True)
 # pca = Pln(counts, batch_size = None)
 # pca.fit(nb_max_iteration=1000, class_optimizer=torch.optim.Rprop)
 # pca = Pln(counts, batch_size = 20)
