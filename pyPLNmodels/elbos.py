fixed an error in the elbo. The gradients works just fine, bt don't find the right parameters

pyPLNmodels/elbos.py
@@ -1,5 +1,5 @@
 import torch  # pylint:disable=[C0114]
-from ._utils import _log_stirling, _trunc_log
+from ._utils import _log_stirling, _trunc_log, closed_form_latent_prob
 from ._closed_forms import _closed_formula_covariance, _closed_formula_coef
 
 from typing import Optional
@@ -7,8 +7,8 @@ from typing import Optional
 
 def elbo_pln(
     endog: torch.Tensor,
-    offsets: torch.Tensor,
     exog: Optional[torch.Tensor],
+    offsets: torch.Tensor,
     latent_mean: torch.Tensor,
     latent_sqrt_var: torch.Tensor,
     covariance: torch.Tensor,
@@ -42,24 +42,36 @@ def elbo_pln(
     n_samples, dim = endog.shape
     s_rond_s = torch.square(latent_sqrt_var)
     offsets_plus_m = offsets + latent_mean
+    Omega = torch.inverse(covariance)
     if exog is None:
         XB = torch.zeros_like(endog)
     else:
         XB = exog @ coef
+    # print('XB:', XB)
     m_minus_xb = latent_mean - XB
-    d_plus_minus_xb2 = (
-        torch.diag(torch.sum(s_rond_s, dim=0)) + m_minus_xb.T @ m_minus_xb
+    m_moins_xb_outer = torch.mm(m_minus_xb.T, m_minus_xb)
+    A = torch.exp(offsets_plus_m + s_rond_s / 2)
+    first_a = torch.sum(endog * offsets_plus_m)
+    sec_a = -torch.sum(A)
+    third_a = -torch.sum(_log_stirling(endog))
+    a = first_a + sec_a + third_a
+    diag = torch.diag(torch.sum(s_rond_s, dim=0))
+    elbo = torch.clone(a)
+    b = -0.5 * n_samples * torch.logdet(covariance) + torch.sum(
+        -1 / 2 * Omega * m_moins_xb_outer
     )
-    elbo = -0.5 * n_samples * torch.logdet(covariance)
-    elbo += torch.sum(
-        endog * offsets_plus_m
-        - 0.5 * torch.exp(offsets_plus_m + s_rond_s)
-        + 0.5 * torch.log(s_rond_s)
-    )
-    elbo -= 0.5 * torch.trace(torch.inverse(covariance) @ d_plus_minus_xb2)
-    elbo -= torch.sum(_log_stirling(endog))
-    elbo += 0.5 * n_samples * dim
-    return elbo / n_samples
+    # print('true b:', true_b)
+    # other_b =
+    elbo += b
+    d = n_samples * dim / 2 + torch.sum(+0.5 * torch.log(s_rond_s))
+    elbo += d
+    f = -0.5 * torch.trace(torch.inverse(covariance) @ diag)
+    elbo += f
+    print("a pln", a)
+    print("b pln", b)
+    print("d pln", d)
+    print("f pln", f)
+    return elbo  # / n_samples
 
 
 def profiled_elbo_pln(
@@ -214,6 +226,94 @@ def elbo_zi_pln(
     n_samples, dim = counts.shape
     s_rond_s = torch.multiply(latent_var, latent_var)
     o_plus_m = offsets + latent_mean
+    if covariates is None:
+        XB = torch.zeros_like(counts)
+        x_coef_inflation = torch.zeros_like(counts)
+    else:
+        XB = covariates @ coef
+        x_coef_inflation = covariates @ _coef_inflation
+
+    m_minus_xb = latent_mean - XB
+
+    A = torch.exp(o_plus_m + s_rond_s / 2)
+    inside_a = torch.multiply(
+        1 - latent_prob, torch.multiply(counts, o_plus_m) - A - _log_stirling(counts)
+    )
+    a = torch.sum(inside_a)
+
+    Omega = torch.inverse(covariance)
+
+    m_moins_xb_outer = torch.mm(m_minus_xb.T, m_minus_xb)
+    un_moins_rho = 1 - latent_prob
+    un_moins_rho_m_moins_xb = un_moins_rho * m_minus_xb
+    un_moins_rho_m_moins_xb_outer = un_moins_rho_m_moins_xb.T @ un_moins_rho_m_moins_xb
+    inside_b = -1 / 2 * Omega * un_moins_rho_m_moins_xb_outer
+    b = -n_samples / 2 * torch.logdet(covariance) + torch.sum(inside_b)
+
+    inside_c = torch.multiply(latent_prob, x_coef_inflation) - torch.log(
+        1 + torch.exp(x_coef_inflation)
+    )
+    c = torch.sum(inside_c)
+
+    log_diag = torch.log(torch.diag(covariance))
+    log_S_term = torch.sum(
+        torch.multiply(1 - latent_prob, torch.log(torch.abs(latent_var))), axis=0
+    )
+    y = torch.sum(latent_prob, axis=0)
+    covariance_term = 1 / 2 * torch.log(torch.diag(covariance)) * y
+    inside_d = covariance_term + log_S_term
+    d = n_samples * dim / 2 + torch.sum(inside_d)
+
+    inside_e = torch.multiply(latent_prob, _trunc_log(latent_prob)) + torch.multiply(
+        1 - latent_prob, _trunc_log(1 - latent_prob)
+    )
+    e = -torch.sum(inside_e)
+    sum_un_moins_rho_s2 = torch.sum(torch.multiply(1 - latent_prob, s_rond_s), axis=0)
+    diag_sig_sum_rho = torch.multiply(
+        torch.diag(covariance), torch.sum(latent_prob, axis=0)
+    )
+    K = sum_un_moins_rho_s2 + diag_sig_sum_rho
+    inside_f = torch.diag(Omega) * K
+    f = -1 / 2 * torch.sum(inside_f)
+    elbo = a + b + c + d + e + f
+    return elbo
+
+
+def full_elbo_zi_pln(
+    counts,
+    covariates,
+    offsets,
+    latent_mean,
+    latent_var,
+    components,
+    coef,
+    _coef_inflation,
+    dirac,
+):
+    """Compute the ELBO (Evidence LOwer Bound) for the Zero Inflated PLN model.
+    See the doc for more details on the computation.
+
+    Args:
+        counts: torch.tensor. Counts with size (n,p)
+        0: torch.tensor. Offset, size (n,p)
+        covariates: torch.tensor. Covariates, size (n,d)
+        latent_mean: torch.tensor. Variational parameter with size (n,p)
+        latent_var: torch.tensor. Variational parameter with size (n,p)
+        pi: torch.tensor. Variational parameter with size (n,p)
+        covariance: torch.tensor. Model parameter with size (p,p)
+        coef: torch.tensor. Model parameter with size (d,p)
+        _coef_inflation: torch.tensor. Model parameter with size (d,p)
+    Returns:
+        torch.tensor of size 1 with a gradient.
+    """
+    covariance = components @ (components.T)
+    latent_prob = closed_form_latent_prob(covariates, coef, covariance, dirac)
+    if torch.norm(latent_prob * dirac - latent_prob) > 0.00000001:
+        print("Bug")
+        raise RuntimeError("rho error")
+    n_samples, dim = counts.shape
+    s_rond_s = torch.multiply(latent_var, latent_var)
+    o_plus_m = offsets + latent_mean
     m_minus_xb = latent_mean - torch.mm(covariates, coef)
     x_coef_inflation = torch.mm(covariates, _coef_inflation)
 
@@ -227,8 +327,9 @@ def elbo_zi_pln(
 
     m_moins_xb_outer = torch.mm(m_minus_xb.T, m_minus_xb)
     un_moins_rho = 1 - latent_prob
-    un_moins_rho_outer = torch.mm(un_moins_rho.T, un_moins_rho)
-    inside_b = -1 / 2 * Omega * un_moins_rho_outer * m_moins_xb_outer
+    un_moins_rho_m = un_moins_rho * latent_mean
+    un_moins_rho_m_outer = un_moins_rho_m.T @ un_moins_rho_m
+    inside_b = -1 / 2 * Omega * un_moins_rho_m_outer
     b = -n_samples / 2 * torch.log(torch.det(covariance)) + torch.sum(inside_b)
 
     inside_c = torch.multiply(latent_prob, x_coef_inflation) - torch.log(

pyPLNmodels/models.py
@@ -21,7 +21,13 @@ from ._closed_forms import (
     _closed_formula_covariance,
     _closed_formula_pi,
 )
-from .elbos import elbo_plnpca, elbo_zi_pln, profiled_elbo_pln
+from .elbos import (
+    elbo_plnpca,
+    elbo_zi_pln,
+    profiled_elbo_pln,
+    full_elbo_zi_pln,
+    elbo_pln,
+)
 from ._utils import (
     _PlotArgs,
     _format_data,
@@ -400,14 +406,14 @@ class _model(ABC):
         loss.backward()
         if self._NAME == "ZIPln":
             print("elbo:", -loss / self.n_samples)
-            print("diff theta:", torch.norm(self.grad_theta() + self._coef.grad))
-            print(
-                "diff theta_0:",
-                torch.norm(self.grad_theta_0() + self._coef_inflation.grad),
-            )
-            print("diff C:", torch.norm(self.grad_C() + self._components.grad))
-            print("diff M:", torch.norm(self.grad_M() + self._latent_mean.grad))
-            print("diff S:", torch.norm(self.grad_S() + self._latent_sqrt_var.grad))
+            # print("diff theta:", torch.norm(self.grad_theta() + self._coef.grad))
+            # print(
+            # "diff theta_0:",
+            # torch.norm(self.grad_theta_0() + self._coef_inflation.grad),
+            # )
+            # print("diff C:", torch.norm(self.grad_C() + self._components.grad))
+            # print("diff M:", torch.norm(self.grad_M() + self._latent_mean.grad))
+            # print("diff S:", torch.norm(self.grad_S() + self._latent_sqrt_var.grad))
             print("diff rho:", torch.norm(self.grad_rho() + self._latent_prob.grad))
         self.optim.step()
         self._update_closed_forms()
@@ -683,12 +689,14 @@ class _model(ABC):
         name_file : str, optional
             The name of the file to save. Defaults to "".
         """
+        cov = self.covariance.cpu() * (torch.eye(self.dim) == 0)
+        # cov = self.covariance
         if self.dim > 400:
             warnings.warn("Only displaying the first 400 variables.")
             sigma = sigma[:400, :400]
-            sns.heatmap(self.covariance[:400, :400].cpu(), ax=ax)
+            sns.heatmap(cov[:400, :400].cpu(), ax=ax)
         else:
-            sns.heatmap(self.covariance.cpu() * (torch.eye(self.dim) == 0), ax=ax)
+            sns.heatmap(cov, ax=ax)
         ax.set_title("Covariance Matrix")
         plt.legend()
         if savefig:
@@ -3276,6 +3284,7 @@ class ZIPln(_model):
             torch.empty(self.n_samples, self.dim).uniform_(0, 1).to(DEVICE)
             * self._dirac
         )
+        # self._latent_prob *=0
 
     def _smart_init_latent_parameters(self):
         self._random_init_latent_parameters()
@@ -3285,6 +3294,26 @@ class ZIPln(_model):
         return self.latent_mean, self.latent_prob
 
     def compute_elbo(self):
+        # elbo_pln(
+        #     self._endog,
+        #     self._exog,
+        #     self._offsets,
+        #     self._latent_mean,
+        #     self._latent_sqrt_var,
+        #     self._covariance,
+        #     self._coef,
+        # )
+        # return full_elbo_zi_pln(
+        #     self._endog,
+        #     self._exog,
+        #     self._offsets,
+        #     self._latent_mean,
+        #     self._latent_sqrt_var,
+        #     self._components,
+        #     self._coef,
+        #     self._coef_inflation,
+        #     self._dirac,
+        # )
         return elbo_zi_pln(
             self._endog,
             self._exog,
@@ -3304,21 +3333,23 @@ class ZIPln(_model):
 
     @property
     def _list_of_parameters_needing_gradient(self):
-        return [
+        list_param = [
             self._latent_mean,
             self._latent_sqrt_var,
-            self._coef_inflation,
-            self._coef,
+            self._latent_prob,
             self._coef_inflation,
             self._components,
-            self._latent_prob,
         ]
+        if self._exog is not None:
+            list_param.append(self._coef)
+        return list_param
 
     @property
     def covariance(self):
         return self._covariance.detach().cpu()
 
     def _update_closed_forms(self):
+        # pass
         # print('latent_prob grad before updating', self._latent_prob.grad)
         with torch.no_grad():
             # self._latent_prob = self._latent_prob
@@ -3368,10 +3399,6 @@ class ZIPln(_model):
                 self._offsets + self._latent_mean + self.latent_sqrt_var**2 / 2
             )
         )
-        MmoinsXB = self._latent_mean - self._exog @ self._coef
-        second = -MmoinsXB @ (
-            ((un_moins_prob.T) @ un_moins_prob) * torch.inverse(self._covariance)
-        )
         return first + second
 
     def grad_S(self):
@@ -3392,11 +3419,10 @@ class ZIPln(_model):
 
     def grad_theta(self):
         un_moins_prob = 1 - self._latent_prob
-        return (
-            -self._exog.T
-            @ (self._exog @ self._coef - self._latent_mean)
-            @ (torch.inverse(self._covariance) * (un_moins_prob.T @ un_moins_prob))
-        )
+        MmoinsXB = self._latent_mean - self._exog @ self._coef
+        A = (un_moins_prob * MmoinsXB) @ torch.inverse(self._covariance)
+        second = -un_moins_prob * A
+        return -self._exog.T @ second
 
     def grad_theta_0(self):
         return self._exog.T @ self._latent_prob - self._exog.T @ (
@@ -3416,10 +3442,10 @@ class ZIPln(_model):
             * torch.inverse(self._components @ (self._components.T))
             @ self._components
         )
-        b_grad = (
-            deter
-            + omega @ (un_moins_rho_outer * m_moins_xb_outer) @ omega @ self._components
+        sec_part_b_grad = (
+            omega @ (un_moins_rho_outer * m_moins_xb_outer) @ omega @ self._components
         )
+        b_grad = deter + sec_part_b_grad
 
         diag = torch.diag(self.covariance)
         rho_t_unn = torch.sum(self._latent_prob, axis=0)
@@ -3427,17 +3453,32 @@ class ZIPln(_model):
         K = torch.sum(un_moins_rho * self._latent_sqrt_var**2, axis=0) + diag * (
             rho_t_unn
         )
-        # K = torch.full((self.dim,),1.)
         first_part_grad = omega @ torch.diag_embed(K) @ omega @ self._components
         x = torch.diag(omega) * rho_t_unn
         second_part_grad = -torch.diag_embed(x) @ self._components
         y = rho_t_unn
-        # y = torch.full((self.dim,),1.)
         first = torch.multiply(y, 1 / torch.diag(self.covariance)).unsqueeze(1)
         second = torch.full((1, self.dim), 1.0)
         Diag = (first * second) * torch.eye(self.dim)
         last_grad = Diag @ self._components
-        return b_grad + first_part_grad + second_part_grad + last_grad
+        grad = b_grad + first_part_grad + second_part_grad + last_grad
+        ## if we want to see the grad of the diagonal of Sigma
+        # def sig_grad_interesting(x):
+        #     return torch.diag(x@(x.T))
+        # grad_sigma = sig_grad_interesting(grad)
+        # deter_sigma = sig_grad_interesting(deter)
+        # sec_part_b_grad_sigma = sig_grad_interesting(sec_part_b_grad)
+        # first_part_grad_sigma = sig_grad_interesting(first_part_grad)
+        # second_part_grad_sigma = sig_grad_interesting(second_part_grad)
+        # last_grad_sigma = sig_grad_interesting(last_grad)
+        # norm = torch.norm(grad_sigma)
+        # print('deter:', torch.norm(deter_sigma)/norm)
+        # print('sec_part b grad', torch.norm(sec_part_b_grad_sigma)/norm)
+        # print('first_part:', torch.norm(first_part_grad_sigma)/norm)
+        # print('second_part_grad:', torch.norm(second_part_grad_sigma)/norm)
+        # print('last_grad:', torch.norm(last_grad_sigma)/norm)
+        # print('--------------')
+        return grad
 
     def grad_rho(self):
         omega = torch.inverse(self._covariance)
@@ -3448,11 +3489,10 @@ class ZIPln(_model):
             + A
             + _log_stirling(self._endog)
         )
-        MmoinsXB = self._latent_mean - self._exog @ self._coef
         un_moins_prob = 1 - self._latent_prob
-        second = un_moins_prob @ (
-            ((MmoinsXB.T) @ MmoinsXB) * torch.inverse(self._covariance)
-        )
+        MmoinsXB = self._latent_mean - self._exog @ self._coef
+        A = (un_moins_prob * MmoinsXB) @ torch.inverse(self._covariance)
+        second = MmoinsXB * A
         third = self._exog @ self._coef_inflation
         fourth_first = -torch.log(torch.abs(self._latent_sqrt_var))
         fourth_second = (

test.py
@@ -9,26 +9,34 @@ import torch
 os.chdir("./pyPLNmodels/")
 
 
-# (
-#     endog,
-#     exog,
-#     offsets,
-#     true_Sigma,
-#     true_beta,
-#     true_infla,
-# ) = get_simulated_count_data(return_true_param=True, n_samples=200, zero_inflated=True)
-endog = get_real_count_data()
-endog = torch.from_numpy(endog)
-
+(
+    endog,
+    exog,
+    offsets,
+    true_Sigma,
+    true_beta,
+    true_infla,
+) = get_simulated_count_data(
+    return_true_param=True, n_samples=200, zero_inflated=True, nb_cov=1, add_const=True
+)
+# endog = get_real_count_data()
+# endog = torch.from_numpy(endog)
+# offsets = None
+# exog = None
 print("percentage zeros:", torch.sum(endog == 0) / (endog.shape[0] * endog.shape[1]))
 # full = ZIPln(endog=endog, exog=exog, offsets=offsets)
-full = ZIPln(endog)
-full.fit(nb_max_iteration=100)
+full = ZIPln(endog, offsets=offsets, exog=exog, add_const=True)
+full.fit(nb_max_iteration=200)
 # print(full)
 full.show()
-beta_zi = full._coef.detach()
+# beta_zi = full._coef.detach()
+print("mse:", torch.mean((full._coef - true_beta) ** 2))
+print("mse:", torch.mean((full._coef_inflation - true_infla) ** 2))
+sns.heatmap(true_Sigma * (1 - torch.eye(true_Sigma.shape[0])))
+plt.show()
 
 
-pln = Pln(endog=endog)
-pln.fit()
-print("mse :", torch.mean((pln._coef - beta_zi) ** 2))
+# pln = Pln(endog=endog)
+# pln.fit()
+# pln.show()
+# print("mse :", torch.mean((pln._coef - beta_zi) ** 2))