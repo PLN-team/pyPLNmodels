tried to debug and fixed the derivative of the lambert function (bug when zero was in input).

pyPLNmodels/_utils.py
@@ -141,12 +141,12 @@ def sample_pln(pln_param, *, seed: int = None, return_latent=False) -> torch.Ten
         ksi = torch.bernoulli(1 / (1 + torch.exp(-zero_inflated_mean)))
     else:
         ksi = 0
-
-    endog = (1 - ksi) * torch.poisson(parameter)
+    Y = torch.poisson(parameter)
+    endog = (1 - ksi) * Y
 
     torch.random.set_rng_state(prev_state)
     if return_latent is True:
-        return endog, gaussian, ksi
+        return endog, gaussian, ksi, Y
     return endog
 
 
@@ -513,7 +513,7 @@ def _get_simulation_coef_cov_offsets_coefzi(
         coef_inflation = None
     else:
         coef = torch.randn(exog.shape[1], dim, device="cpu")
-        coef += 0
+        # coef += 4
         if zero_inflated is True:
             coef_inflation = torch.randn(exog.shape[1], dim, device="cpu")
         else:
@@ -1084,11 +1084,9 @@ class lambert(torch.autograd.Function):
         """
         (input,) = ctx.saved_tensors
         arr = input.numpy()
-        print("sum 0 ", np.sum(np.abs(arr) < 1e-12))
         lamb = lambertw(arr).real
-        out = lamb / (input * (1 + lamb))
-        print("sum 1 + lamb", np.sum((1 + lamb) == 0))
-        print("out:", torch.norm(out))
+        # out = lamb / (input * (1 + lamb))
+        out = torch.where(torch.abs(input) > 1e-5, lamb / (input * (1 + lamb)), 1)
         return grad_output * out
 
 

pyPLNmodels/models.py
@@ -40,6 +40,7 @@ from ._utils import (
     _handle_data,
     _add_doc,
     _trunc_log,
+    closed_form_latent_prob,
 )
 
 from ._initialization import (
@@ -48,6 +49,9 @@ from ._initialization import (
     _init_coef,
     _init_latent_mean,
 )
+
+
+NB_BEFORE_SHIFTING = 6000
 
 if torch.cuda.is_available():
     DEVICE = "cuda"
@@ -392,6 +396,30 @@ class _model(ABC):
         self._print_end_of_fitting_message(stop_condition, tol)
         self._fitted = True
 
+    def print_mses(self):
+        print("mse cov:", torch.mean((self.true_covariance - self._covariance) ** 2))
+        print("mse infla:", torch.mean((self.true_infla - self._coef_inflation) ** 2))
+        print("mse beta:", torch.mean((self.true_coef - self._coef) ** 2))
+        print("mse latent_prob:", torch.mean((self._latent_prob - self.ksi) ** 2))
+
+    def save_mse(self):
+        self.mse_cov_list.append(
+            torch.mean((self.true_covariance - self._covariance) ** 2).detach()
+        )
+        self.mse_infla_list.append(
+            torch.mean((self.true_infla - self._coef_inflation) ** 2).detach()
+        )
+        self.mse_beta_list.append(
+            torch.mean((self.true_coef - self._coef) ** 2).detach()
+        )
+        if self.use_closed_form_prob is True:
+            latent_prob = closed_form_latent_prob(
+                self._exog, self._coef, self._covariance, self._dirac
+            )
+        else:
+            latent_prob = self._latent_prob
+        self.mse_ksi_list.append(torch.mean((latent_prob - self.ksi) ** 2).detach())
+
     def _trainstep(self):
         """
         Perform a single training step.
@@ -406,6 +434,24 @@ class _model(ABC):
         loss.backward()
         if self._NAME == "ZIPln":
             print("elbo:", -loss / self.n_samples)
+            if self.nb_iteration_done == NB_BEFORE_SHIFTING + 1:
+                print("removing gradients")
+                self.print_mses()
+                self._endog = torch.clone(self.Y_inflated)
+                self.optim = torch.optim.Rprop(
+                    self._list_of_parameters_needing_gradient, lr=0
+                )
+                # self.use_closed_form_prob = True
+                self._dirac = self._endog == 0
+                # latent_prob = closed_form_latent_prob(self._exog, self._coef, self._covariance, self._dirac)
+                with torch.no_grad():
+                    self._latent_prob *= 0
+                    self._coef_inflation *= 0
+            try:
+                self.save_mse()
+            except:
+                pass
+
             # print("diff theta:", torch.norm(self.grad_theta() + self._coef.grad))
             # print(
             # "diff theta_0:",
@@ -693,8 +739,8 @@ class _model(ABC):
         name_file : str, optional
             The name of the file to save. Defaults to "".
         """
-        cov = self.covariance.cpu() * (torch.eye(self.dim) == 0)
-        # cov = self.covariance
+        # cov = self.covariance.cpu() * (torch.eye(self.dim) == 0)
+        cov = self.covariance
         if self.dim > 400:
             warnings.warn("Only displaying the first 400 variables.")
             sigma = sigma[:400, :400]
@@ -3237,6 +3283,8 @@ class ZIPln(_model):
         true_coef=None,
         true_infla=None,
         use_closed_form_prob=False,
+        Y_inflated=None,
+        ksi=None,
     ):
         super().__init__(
             endog,
@@ -3250,10 +3298,13 @@ class ZIPln(_model):
         self.true_covariance = true_covariance
         self.true_coef = true_coef
         self.true_infla = true_infla
-        self.mse_sigma_list = []
-        self.mse_coef_list = []
+        self.mse_cov_list = []
+        self.mse_beta_list = []
         self.mse_infla_list = []
+        self.mse_ksi_list = []
         self.use_closed_form_prob = use_closed_form_prob
+        self.Y_inflated = Y_inflated
+        self.ksi = ksi
 
     @property
     def _description(self):
@@ -3354,6 +3405,8 @@ class ZIPln(_model):
             list_param.append(self._latent_prob)
         if self._exog is not None:
             list_param.append(self._coef)
+        if self.nb_iteration_done > NB_BEFORE_SHIFTING:
+            return [self._coef_inflation, self._latent_prob]
         return list_param
 
     @property
@@ -3563,4 +3616,8 @@ class ZIPln(_model):
             )
         )
         sixth = sixth_first + sixth_second
-        return first + second + third + fourth + fifth + sixth
+        full_diag_omega = torch.diag(omega).expand(self.exog.shape[0], -1)
+        seventh = (
+            -1 / 2 * (1 - 2 * self._latent_prob) * (MmoinsXB) ** 2 * (full_diag_omega)
+        )
+        return first + second + third + fourth + fifth + sixth + seventh

test.py
@@ -2,6 +2,7 @@ from pyPLNmodels.models import PlnPCAcollection, PlnPCA, Pln, ZIPln
 from pyPLNmodels import get_real_count_data, get_simulated_count_data
 
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 import os
 import torch
@@ -19,7 +20,10 @@ add_const = True
 # ) = get_simulated_count_data(
 #     return_true_param=True, n_samples=200, zero_inflated=True, nb_cov=0, add_const=add_const, return_latent_variables = True
 # )
+# Y = simu[3]
+# ksi = simu[2]
 # endog = simu[0]
+
 endog = get_real_count_data()
 endog = torch.from_numpy(endog)
 offsets = None
@@ -27,15 +31,45 @@ exog = None
 print("percentage zeros:", torch.sum(endog == 0) / (endog.shape[0] * endog.shape[1]))
 # full = ZIPln(endog=endog, exog=exog, offsets=offsets)
 full = ZIPln(
-    endog, offsets=offsets, exog=exog, add_const=add_const, use_closed_form_prob=False
+    endog,
+    offsets=offsets,
+    exog=exog,
+    add_const=add_const,
+    use_closed_form_prob=True,  # Y_inflated = true_Y, true_covariance= true_Sigma, true_coef=true_beta, true_infla=true_infla, ksi = ksi
 )
-full.fit(nb_max_iteration=1600, tol=0)
-# print(full)
+full.fit(nb_max_iteration=1550, tol=0)
 full.show()
+# print(full)
+# full.show()
 # beta_zi = full._coef.detach()
 print("mse beta:", torch.mean((full._coef - true_beta) ** 2))
 print("mse infla:", torch.mean((full._coef_inflation - true_infla) ** 2))
 print("mse Sigma:", torch.mean((full._covariance - true_Sigma) ** 2))
+
+
+def show_mses(model):
+    absc = np.arange(len(model.mse_cov_list))
+    plt.plot(absc, model.mse_cov_list, label="Sigma")
+    plt.plot(absc, model.mse_beta_list, label="beta")
+    plt.plot(absc, model.mse_infla_list, label="infla")
+    plt.plot(absc, model.mse_ksi_list, label="ksi")
+    plt.legend()
+    plt.yscale("log")
+    plt.show()
+
+
+fig, axes = plt.subplots(2)
+sns.heatmap(full._coef.detach(), ax=axes[0])
+sns.heatmap(true_beta, ax=axes[1])
+plt.show()
+
+fig, axes = plt.subplots(2)
+sns.heatmap(full._coef_inflation.detach(), ax=axes[0])
+sns.heatmap(true_infla, ax=axes[1])
+plt.show()
+show_mses(full)
+
+
 # sns.heatmap(true_Sigma)
 # plt.show()
 