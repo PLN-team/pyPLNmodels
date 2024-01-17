45361ad49ca8d2c3b3d4dba713afeb6aa51ae8b7
bastien-mva <bastien.batardiere@gmail.com>
2024-01-17 17:32:57 +0100

    add a test that see if the initialization contains zeros.

--- tests/test_common.py
+++ tests/test_common.py
@@ -124,3 +124,7 @@ def test_batch(model):
         assert mse_coef < 0.1
     elif model.nb_cov == 0:
         assert model.coef is None
+
+
+def test_raise_error_on_zero_counts():
+    model = Pln()
