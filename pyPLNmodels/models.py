fix for the commit, the gradients were vanishing.

pyPLNmodels/models.py
@@ -3777,10 +3777,10 @@ class ZIPln(_model):
         if self._use_closed_form_prob is False:
             with torch.no_grad():
                 self._latent_prob = torch.maximum(
-                    self._latent_prob, torch.tensor([0]).to(DEVICE)
+                    self._latent_prob, torch.tensor([0]), out=self._latent_prob
                 )
                 self._latent_prob = torch.minimum(
-                    self._latent_prob, torch.tensor([1]).to(DEVICE)
+                    self._latent_prob, torch.tensor([1]), out=self._latent_prob
                 )
                 self._latent_prob *= self._dirac
 
@@ -3910,17 +3910,6 @@ class ZIPln(_model):
             list_parameters.append(self._coef)
             list_parameters.append(self._coef_inflation)
         return list_parameters
-
-    def _update_closed_forms(self):
-        if self.use_closed_form_prob is False:
-            with torch.no_grad():
-                self._latent_prob = torch.maximum(
-                    self._latent_prob, torch.tensor([0]), out=self._latent_prob
-                )
-                self._latent_prob = torch.minimum(
-                    self._latent_prob, torch.tensor([1]), out=self._latent_prob
-                )
-                self._latent_prob *= self._dirac
 
     @property
     @_add_doc(_model)