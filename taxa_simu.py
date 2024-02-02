e48cff38c56005bd5f0bffb7932c7e322d670d56
bastien-mva <bastien.batardiere@gmail.com>
2024-02-02 12:20:39 +0100

    fit the taxa simulations.

---
+++ taxa_simu.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+import numpy as np
+import torch
+from sklearn.preprocessing import OneHotEncoder
+from pyPLNmodels import ZIPln
+
+print(os.getcwd())
+counts = pd.read_csv("data_mahendra/counts.tsv", delimiter="\t").drop(
+    columns=["Sample"]
+)
+cov = pd.read_csv("data_mahendra/metadata.tsv", delimiter="\t")[
+    ["site", "time", "lineage"]
+]
+encoder = OneHotEncoder()
+hot_cov = encoder.fit_transform(cov).toarray()
+zi = ZIPln(counts, exog=hot_cov, offsets=None)
+zi.fit(verbose=True, do_smart_init=False)
