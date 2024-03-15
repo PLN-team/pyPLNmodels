d6402af50bcb97766959111beb292b69d9d63cbd
bastien-mva <bastien.batardiere@gmail.com>
2024-03-15 09:35:49 +0100

    try to see what is going around the ZIPln models.

--- inves.py
+++ inves.py
@@ -1,74 +1,51 @@
-from pyPLNmodels import Pln, ZIPln, get_simulation_parameters, sample_zipln
+from pyPLNmodels import (
+    Pln,
+    ZIPln,
+    load_microcosm,
+    load_model,
+    get_zipln_simulated_count_data,
+)
+from pyPLNmodels.models import Brute_ZIPln
 import torch
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+# dim = 97 -> 98 pour le standard sans forme close pour la proba (i.e. toutes les formes closes possibles)
+# 61 pour le standard avec global
+endog, exog = load_microcosm(dim=200, n_samples=500)
+exog_inflation = exog
+offsets = None
+# endog, exog, exog_inflation, offsets = get_zipln_simulated_count_data()
+nb_iter = 1000
 
-n_samples = 1000
-parameters = get_simulation_parameters(
-    zero_inflation_formula="global", n_samples=n_samples, dim=200
-)
-
-parameters._set_gaussian_mean(2)
-parameters._set_mean_proba(0.3)
-
-exog = parameters.exog
-exog_inflation = parameters.exog_inflation
-offsets = parameters.offsets
-true_Sigma = parameters.covariance
-true_Omega = torch.inverse(true_Sigma)
-true_coef = parameters.coef
-
-nb_iter = 400
-endog_zi, endog_pln = sample_zipln(parameters, return_pln=True)
+dict_init = None
 
 
-print(
-    "percentage of zeros for the zero inflated data",
-    torch.mean((endog_zi == 0).float()),
-)
-print(
-    "percentage of zeros for the non inflated data",
-    torch.mean((endog_pln == 0).float()),
-)
-
-zi = ZIPln(endog_zi, exog=exog, zero_inflation_formula="global", offsets=offsets)
-zi.fit(tol=0, nb_max_iteration=nb_iter)
-
-fairpln = Pln(endog_pln, exog=exog, offsets=offsets)
-fairpln.fit(tol=0, nb_max_iteration=nb_iter)
-
-pln = Pln(endog_zi, exog=exog, offsets=offsets)
-pln.fit(tol=0, nb_max_iteration=nb_iter)
-
-fig, axes = plt.subplots(2, 2, figsize=(10, 20))
-
-
-def rmse(t):
-    return np.round(torch.sqrt(torch.mean(t**2)), 4)
+def show_model(model):
+    # dict_init = load_model("zi")
+    # mod = model(endog, exog = exog, exog_inflation = exog_inflation, offsets = offsets, dict_initialization = dict_init, use_closed_form_prob = True)
+    mod = model(
+        endog,
+        exog=None,
+        exog_inflation=None,
+        offsets=offsets,
+        dict_initialization=dict_init,
+        use_closed_form_prob=True,
+    )
+    # mod = model(endog,zero_inflation_formula = "global", offsets = offsets, dict_initialization = dict_init, use_closed_form_prob = True)
+    mod.fit(verbose=True, lr=0.001, tol=0, nb_max_iteration=nb_iter)
+    not_diagonal = (mod._covariance * (1 - torch.eye(mod.dim))).detach()
+    # sns.heatmap(not_diagonal)
+    # plt.show()
+    print("mean not diagonal", torch.mean((not_diagonal) ** 2))
+    mod.show()
+    mod.save("zi")
 
 
-def check_covariance_coef(model, ax, label):
-    Sigma = model.covariance
-    Omega = torch.inverse(Sigma)
-    coef = model.coef
-    mse_Sigma = rmse(Sigma - true_Sigma)
-    mse_Omega = rmse(Omega - true_Omega)
-    mse_coef = rmse(coef - true_coef)
-    sns.heatmap(Omega, ax=ax)
-    title = rf"{label}__MSE($\Sigma$){mse_Sigma}, MSE($\Omega$){mse_Omega}, MSE($B$){mse_coef}"
-    ax.set_title(title)
-
-
-sns.heatmap(true_Omega, ax=axes[0, 0])
-axes[0, 0].set_title("True Omega")
-check_covariance_coef(zi, axes[1, 0], label="ZIPln on ZIPln data")
-check_covariance_coef(fairpln, axes[0, 1], label="Pln on pln data.")
-check_covariance_coef(pln, axes[1, 1], label="Pln on ZIpln data.")
-
-fig.suptitle("Comparison of estimation of Omegas")
-plt.show()
+show_model(ZIPln)
+# show_model(Brute_ZIPln)
+# zi.save("zimodel")
 
 
 # endog, exog, offsets = get_zipln_simulated_count_data()
