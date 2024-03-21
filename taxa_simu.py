783d93e95646bc37511757dcf8d8088eafcc46fe
bastien-mva <bastien.batardiere@gmail.com>
2024-03-21 13:31:14 +0100

    useless lines.

--- taxa_simu.py
+++ taxa_simu.py
@@ -13,40 +13,50 @@ from sklearn.preprocessing import OneHotEncoder
 # cov = pd.read_csv("data_mahendra/metadata.tsv", delimiter="\t")[
 #     ["site", "time", "lineage"]
 # ]
-cov_list = ["site", "lineage", "time", "Sample"]
-endog_stream = pkg_resources.resource_stream(
-    __name__, "pyPLNmodels/data/microcosm/counts.tsv"
-)
-endog = pd.read_csv(endog_stream, delimiter="\t")
-cov_stream = pkg_resources.resource_stream(
-    __name__, "pyPLNmodels/data/microcosm/metadata.tsv"
-)
-cov = pd.read_csv(cov_stream, delimiter="\t")[cov_list]
-endog = endog.drop(columns="Sample")
-sample_names = cov["Sample"]
-cov = cov.drop(columns="Sample")
+# cov_list = ["site", "lineage", "time", "Sample"]
+# endog_stream = pkg_resources.resource_stream(
+#     __name__, "pyPLNmodels/data/microcosm/counts.tsv"
+# )
+# endog = pd.read_csv(endog_stream, delimiter="\t")
+# cov_stream = pkg_resources.resource_stream(
+#     __name__, "pyPLNmodels/data/microcosm/metadata.tsv"
+# )
+# cov = pd.read_csv(cov_stream, delimiter="\t")[cov_list]
+# endog = endog.drop(columns="Sample")
+# sample_names = cov["Sample"]
+# cov = cov.drop(columns="Sample")
 
-data = {}
-for name in cov.columns:
-    encoder = OneHotEncoder(drop="first")
-    hot = torch.from_numpy(encoder.fit_transform(cov).toarray())
-    data[name] = hot
-encoder = OneHotEncoder(drop="first")
-exog = encoder.fit_transform(cov).toarray()
+# data = {}
+# for name in cov.columns:
+#     encoder = OneHotEncoder(drop="first")
+#     hot = torch.from_numpy(encoder.fit_transform(cov).toarray())
+#     data[name] = hot
+# encoder = OneHotEncoder(drop="first")
+# exog = encoder.fit_transform(cov).toarray()
 
-columns = endog.columns
-dict_init_enh = load_model("zi_model")
-# dict_init = None
-zi = ZIPln(
-    endog,
-    exog=exog,
-    exog_inflation=exog,
-    zero_inflation_formula="column-wise",
-    dict_initialization=dict_init_enh,
-)
-zibrute = ZIPln(
-    endog, exog, exog_inflation=exog_inflation, zero_inflation_formula="column-wise"
-)
+# columns = endog.columns
+# dict_init = load_model("zi_inter")
+data = load_microcosm(get_interaction=False, n_samples=2000, dim=2000, for_formula=True)
+dict_init = None
+zi = ZIPln.from_formula("endog ~ 1 + exog", data=data, dict_initialization=dict_init)
+zi.fit(tol=0, nb_max_iteration=100, verbose=True)
+zi.show()
+print("loglike", zi.loglike)
+print("cov", zi._covariance)
+# pd.DataFrame(zi.proba_inflation).to_csv("zi_inter/proba_inflation.csv")
+# pd.DataFrame(zi.exog).to_csv("zi_inter/covariates.csv")
+# data["exog"].to_csv("zi_inter/covariates_with_names.csv")
+
+# zi.fit(tol = 0, nb_max_iteration = 4000, verbose = True)
+# zi.save("zi_inter")
+# zi.show()
+# print('exog', zi.exog)
+# print(' exog shape', zi.exog.shape)
+# print('exog infla', zi.exog_inflation)
+# print('exog infla shape', zi.exog_inflation.shape)
+# zibrute = ZIPln(
+#     endog, exog, exog_inflation=exog_inflation, zero_inflation_formula="column-wise"
+# )
 
 # print('elbo', zi.elbo)
 # zi.fit(nb_max_iteration=500, verbose=True, do_smart_init = True, tol = 0, lr = 0.00000001)
