835ebe17d53474d57c7661ad7bbc53259b53600a
bastien-mva <bastien.batardiere@gmail.com>
2024-04-11 21:24:19 +0200

    SAVE COMPARISON FILES

--- pln.py
+++ pln.py
@@ -2,36 +2,77 @@ import pandas as pd
 import numpy as np
 import torch
 
-from pyPLNmodels import Pln, ZIPln, Brute_ZIPln
+from pyPLNmodels import Pln
+from pyPLNmodels.models import Pln_0
+from pyPLNmodels.elbos import elbo_pln, elbo_pln_0, r_elbo_pln_0
 
 
+## microcosm data
 data = {}
 data["endog"] = pd.read_csv("reduced/endog.csv").drop(columns="Unnamed: 0")
 p = data["endog"].shape[1]
 data["site"] = pd.read_csv("reduced/site.csv").drop(columns="Unnamed: 0").squeeze()
 data["time"] = pd.read_csv("reduced/time.csv").drop(columns="Unnamed: 0").squeeze()
-
-
-best = (data["endog"] > 0).mean(axis=0) > 0.05
+data["site_time"] = data["time"] + data["site"]
+best = (data["endog"] > 0).mean(axis=0) > 0.30
 data["endog"] = data["endog"].loc[:, best]
-
-
-print("data shape", data["endog"].shape)
-
-data["offsets"] = np.log(
-    pd.read_csv("reduced/offsets.csv")
-    .drop(columns="Unnamed: 0")
-    .values.repeat(p, axis=1)
-)[:, best]
-
-zi = Brute_ZIPln.from_formula(
-    "endog ~ 1", data=data, zero_inflation_formula="global", use_closed_form_prob=False
+data["offsets"] = (
+    np.log(
+        pd.read_csv("reduced/offsets.csv")
+        .drop(columns="Unnamed: 0")
+        .values.repeat(p, axis=1)
+    )[:, best]
+    * 0
 )
-zi.fit(verbose=True)
-print(zi)
-print(zi.proba_inflation)
 
 
-# pln = Pln.from_formula("endog ~ 1", data=data)
-# pln.fit()
-# print(pln)
+pln = Pln.from_formula("endog ~ 1", data=data)
+n = pln.n_samples
+pln.fit(verbose=True)
+print(pln)
+
+X = pln.exog
+
+M = torch.from_numpy(
+    pd.read_csv("no_covariates_M_20779.csv").drop(columns="Unnamed: 0").values
+)
+S = torch.from_numpy(
+    pd.read_csv("no_covariates_S_20779.csv").drop(columns="Unnamed: 0").values
+)
+B = torch.from_numpy(
+    pd.read_csv("no_covariates_beta_20779.csv").drop(columns="Unnamed: 0").values
+)
+covariance = torch.from_numpy(
+    pd.read_csv("no_covariates_cov_20779.csv").drop(columns="Unnamed: 0").values
+)
+
+
+print(
+    "my own loglike",
+    n
+    * r_elbo_pln_0(
+        pln.endog,
+        pln.exog,
+        pln.offsets,
+        pln.latent_mean - pln.exog @ pln.coef,
+        pln.latent_sqrt_var,
+        pln.covariance,
+        pln.coef,
+    ),
+)
+# print('my own loglike centered in 0', n*r_elbo_pln_0(pln.endog, pln.exog, pln.offsets, pln.latent_mean - pln.exog$pln.coef, pln.latent_sqrt_var, pln.covariance, pln.coef))
+print(
+    "r loglike 0",
+    n * r_elbo_pln_0(pln.endog, pln.exog, pln.offsets, M, S, covariance, B),
+)
+# print('r loglike 0 bis', torch.sum(r_elbo_pln_0_bis(pln.endog, pln.exog, pln.offsets, M , S, covariance, B)))
+
+
+# zi = Brute_ZIPln.from_formula(
+#     "endog ~ 1 + site_time", data=data, zero_inflation_formula="global", use_closed_form_prob=False
+# )
+# zi.fit(verbose = True, nb_max_iteration = 1000, tol = 0)
+# zi.show()
+# print(zi)
+# zi.viz(colors = data["time"])
+# zi.viz_position(colors = data["time"])
--- pln.r
+++ pln.r
@@ -1,60 +1,61 @@
+library(devtools)
 library(PLNmodels)
-library(factoextra)
-library(ggplot2)
-
+library(torch)
+# install("../PLNmodels")
 
 microcosm = readRDS("microcosm_reduced.rds")
-abund = microcosm$Abundance
-best <- colMeans(microcosm$Abundance > 0) > 0.05
+best <- colMeans(microcosm$Abundance > 0) > 0.30
 microcosm$Abundance <- microcosm$Abundance[, best]
 microcosm$site_time <- droplevels(microcosm$site_time)
 
-myPLN <- PLN("Abundance~ 1 + site_time +  offset(log(Offset))", data = microcosm)
-# help(PLN)
+n <- dim(microcosm$Abundance)[1]
+p <- dim(microcosm$Abundance)[2]
 
-pdf("pln.pdf")
-## bricolage car quelques points explosent, sans conséquences sur la convergence finale
-obj_PLN <- -myPLN$optim_par$objective
-obj_PLN
-plot(obj_PLN, ## optimisation nlopt de la vraisemblance profilée
-     ylim = c(obj_PLN[1], # fonction objective sans les constantes
-              tail(obj_PLN,1)), log = "xy")
+myPLN <- PLN("Abundance~ 1", data = microcosm, control = PLN_param(backend = "torch", config_optim = list(lr = 0.01, numepoch = 5000, maxeval = 100000, ftolrel = -1, xtolrel = -1)))
 print(myPLN)
-M <- myPLN$latent
-microcosm$site_time
 
+covariance = sigma(myPLN)
+Y_ = microcosm$Abundance
+X = torch_ones(n,1)
+O_ = torch_zeros(n,p)
+M_ = myPLN$var_par$M
+S_ = myPLN$var_par$S
+Omega = torch_inverse(covariance)
+B_ = coef(myPLN)
 
-res <- prcomp(M, center = TRUE, scale = TRUE)
-fviz_pca_ind(res, col.ind = microcosm$site_time)
+Y = torch_tensor(Y_)
+O = torch_tensor(O_)
+M = torch_tensor(M_)
+S = torch_tensor(S_)
+B = torch_tensor(B_)
 
+logfactorial_torch <- function(endog_){
+  endog = torch_clone(endog_)
+  endog[endog_ == 0] <- 1 ## 0! = 1!
+  return(endog*torch_log(endog) - endog + torch_log(8*torch_pow(endog,3) +
+                                                    4*torch_pow(endog,2) +
+                                                    endog + 1/30)/6 +
+         torch_log(pi)/2)
+}
+torch_vloglik <- function(Y__,X,O,M,S,Omega,B) {
+  S2  <- torch_square(S)
+  Z  <- O + M + torch_mm(X, B)
+  A <- torch_exp(Z + .5 * S2)
+  p = Y$shape[2]
+  n = Y$shape[1]
+  Ji_tmp = .5 * torch_logdet(Omega) + torch_sum(Y__ * Z - A + .5 * torch_log(S2), dim = 2) - .5 * torch_sum(torch_mm(M, Omega) * M + S2 * torch_diag(Omega), dim = 2)
+  other <- n/2*torch_logdet(Omega)
+  other = other + torch_sum(Y * Z - A + .5 * torch_log(S2))
+  other = other - .5 * torch_sum(torch_mm(M, Omega) * M + S2 * torch_diag(Omega))
+  Ji <- - torch_sum(logfactorial_torch(Y__), dim = 2) + Ji_tmp
+  Ji <- .5 * p + as.numeric(Ji$cpu())
+  return(Ji)
+}
+elbo = torch_vloglik(Y,X,O,M,S,Omega,B)
+print('ELBO PLN at convergence')
+print(sum(elbo))
 
-plot(myPLN$optim_par$objective, type = "l", log = "xy") ## Les itérations du VEM: la vraisemblance estimée
-dev.off()
-
-myzi <- ZIPLN("Abundance~ 1 +  offset(log(Offset))", data = microcosm)
-# help(PLN)
-
-pdf("zi.pdf")
-## bricolage car quelques points explosent, sans conséquences sur la convergence finale
-obj_zi <- myzi$optim_par$objective
-obj_zi
-plot(obj_zi, ## optimisation nlopt de la vraisemblance profilée
-     ylim = c(obj_zi[1], # fonction objective sans les constantes
-              tail(obj_zi,1)), log = "xy")
-print(myzi)
-plot(myzi$optim_par$objective, type = "l", log = "xy") ## Les itérations du VEM: la vraisemblance estimée
-
-B = myzi$model_par[[1]]
-covariance = sigma(myzi)
-pi = myzi$model_par[[3]]
-
-myzi$var_par[[1]]
-myzi$Abundance
-
-
-options(max.print=3000)
-
-
-
-
-dev.off()
+write.csv(covariance, "no_covariates_cov_20779.csv")
+write.csv(B_, "no_covariates_beta_20779.csv")
+write.csv(M_, "no_covariates_M_20779.csv")
+write.csv(S_, "no_covariates_S_20779.csv")
