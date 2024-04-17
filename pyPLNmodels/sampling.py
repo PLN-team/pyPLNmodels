eb1c350e1778f89e5d1e7ead485b9f349193f72a
bastien-mva <bastien.batardiere@gmail.com>
2024-04-17 10:55:01 +0200

    add message error.

--- pyPLNmodels/sampling.py
+++ pyPLNmodels/sampling.py
@@ -139,6 +139,10 @@ def _get_simulation_coef_cov_offsets_coefzi(
             )
             if add_const_inflation is True:
                 exog_inflation = torch.cat((exog_inflation, torch.ones(1, dim)), axis=0)
+        else:
+            msg = f"Wrong zero_inflation formula. Got {zero_inflation_formula},"
+            msg += ' expected "column-wise" or "row-wise".'
+            raise ValueError(msg)
     if zero_inflation_formula is not None:
         if zero_inflation_formula in ["column-wise", "row-wise"]:
             if nb_cov_inflation > 0:
