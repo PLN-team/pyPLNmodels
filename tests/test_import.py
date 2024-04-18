653dde19926f43f4d6d77260ff0d44b928511ba8
bastien-mva <bastien.batardiere@gmail.com>
2024-04-18 18:13:50 +0200

    test for import fixed.

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,18 +2,10 @@ from pyPLNmodels import load_microcosm
 
 
 def test_microcosm():
-    endog, exog = load_microcosm()
-    assert endog.shape == (300, 200)
-    assert exog.shape == (300, 4)
+    data = load_microcosm(min_perc=0)
+    assert data["endog"].shape == (300, 200)
 
-    endog, exog = load_microcosm(n_samples=50, dim=12)
-    assert endog.shape == (50, 12)
-    assert exog.shape == (50, 3)
-
-
-def test_microcosm_with_affil():
-    endog, exog, affil = load_microcosm(get_affil=True)
-
-
-def test_microcosm_formula():
-    data = load_microcosm(for_formula=True)
+    data = load_microcosm(n_samples=50, dim=12, min_perc=0)
+    assert data["endog"].shape == (50, 12)
+    data = load_microcosm(get_affil=True)
+    assert data["affiliations"] is not None
