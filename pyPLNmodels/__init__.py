11dffff413e5baa72e9e1b7c083994730cff3a9f
bastien-mva <bastien.batardiere@gmail.com>
2024-05-06 10:38:53 +0200

    updated files from zero inflation part

--- pyPLNmodels/__init__.py
+++ pyPLNmodels/__init__.py
@@ -1,6 +1,12 @@
 import importlib.metadata
 
-from .models import PlnPCAcollection, Pln, PlnPCA, ZIPln  # pylint:disable=[C0114]
+from .models import (
+    PlnPCAcollection,
+    Pln,
+    PlnPCA,
+    ZIPln,
+    Brute_ZIPln,
+)  # pylint:disable=[C0114]
 from .oaks import load_oaks
 from .elbos import profiled_elbo_pln, elbo_plnpca, elbo_pln
 from .sampling import (
@@ -10,9 +16,12 @@ from .sampling import (
     sample_zipln,
     get_pln_simulated_count_data,
     get_zipln_simulated_count_data,
-    get_real_count_data,
     get_simulation_parameters,
 )
+
+from .scrna import load_scrna
+from .microcosm import load_microcosm
+
 from .load import load_model, load_plnpcacollection, load_pln, load_plnpca
 
 from ._initialization import log_posterior
@@ -26,7 +35,7 @@ __all__ = (
     "elbo_pln",
     "get_pln_simulated_count_data",
     "get_zipln_simulated_count_data",
-    "get_real_count_data",
+    "load_scrna",
     "load_model",
     "load_plnpcacollection",
     "load_pln",
