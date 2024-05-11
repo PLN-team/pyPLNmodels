c9bcc8586caf4394df94b979a0dd7ed663e239ba
bastien-mva <bastien.batardiere@gmail.com>
2024-05-11 18:58:55 +0200

    add moving average for cleaner functions.

--- pyPLNmodels/_utils.py
+++ pyPLNmodels/_utils.py
@@ -1192,3 +1192,9 @@ def _check_right_exog_inflation_shape(
 
 def mse(t):
     return torch.mean(t**2)
+
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1 :] / n
