05052e95f4308122a1c3606a2700d4216a82723a
bastien-mva <bastien.batardiere@gmail.com>
2024-05-14 19:03:16 +0200

    begin to add the visualization

--- benchmark.py
+++ benchmark.py
@@ -29,26 +29,43 @@ def get_sc_mark_data(max_class=28, max_n=200, dim=100):
     return Y, GT, list(GT_name.values.__array__())
 
 
-def append_running_times_model(Y, model_str):
+def append_running_times_model(Y, model_str, n):
     if model_str == "Pln":
         model = Pln(Y)
         model.fit(nb_max_iteration=2000)
     else:
         model = PlnPCA(Y, rank=rank)
         model.fit(nb_max_iteration=2000)
-    dict_rt[model_str].append(model._criterion_args.running_times[-1])
+    dict_rt[model_str][n].append(model._criterion_args.running_times[-1])
 
 
-def plot_dict(dict_rt, model_str):
+def plot_dict(dict_rt, model_str, ns):
     dict_rt_model = dict_rt[str(model_str)]
-    if model_str == "Pln":
-        color = "blue"
-    else:
-        color = "orange"
-    sns.lineplot(x=ps_plnpca, y=dict_rt_model, color=color, label=f"{model_str}")
+    linestyles = ["--", "solid", "dashed", "dashdot"]
+    for i, n in enumerate(ns):
+        res_model = dict_rt_model[n]
+        if model_str == "Pln":
+            color = "blue"
+        else:
+            color = "orange"
+        sns.lineplot(
+            x=ps_plnpca,
+            y=res_model,
+            color=color,
+            label=f"{model_str}, n = {n}",
+            linestyle=linestyles[i],
+        )
     plt.legend()
     plt.xlabel(r"Number of variables $p$")
     plt.ylabel("Running time")
+
+
+def dict_to_df(model_str):
+    dict_dim = {"dim": ps_plnpca}
+    union_dict = {**dict_rt[model_str], **dict_dim}
+    columns = ns
+    columns.append("dim")
+    return pd.DataFrame.from_dict(union_dict)
 
 
 if __name__ == "__main__":
@@ -58,53 +75,50 @@ if __name__ == "__main__":
     df.to_csv("full_scmark_little.csv")
     df = pd.read_csv("full_scmark_little.csv")
 
-    n = 300
+    ns = [300, 500]
     # p0 = 2500
     # pn = 14059
     # ecart = 300
     fig = plt.figure(figsize=(20, 10))
-    pmax_pln = 500
-    pn = 700
+    pmax_pln = 300
+    pn = 400
     ecart = 100
     rank = 5
     ps_pln = np.arange(100, pmax_pln, ecart)
     ps_plnpca = np.concatenate((ps_pln, np.arange(pmax_pln, pn, ecart)))
-    name_file = (
-        f"n_{n}_nbps_{len(ps_plnpca)}_p0_{pmax_pln}_pn_{pn}_ecart_{ecart}_rank_{rank}"
-    )
     dict_rt = {
-        "Pln": [],
-        "PlnPCA": [],
+        "Pln": {n: [] for n in ns},
+        "PlnPCA": {n: [] for n in ns},
     }
 
-    if True:
-        # if exists(name_file) is False:
-        for p in tqdm(ps_plnpca):
-            Y = df.values[:n, :p]
-            # Y, _, labels = get_sc_mark_data(max_n=n, dim=p)
-            append_running_times_model(Y, "PlnPCA")
-            if p < pmax_pln:
-                append_running_times_model(Y, "Pln")
-            else:
-                dict_rt["Pln"].append(None)
-            print("dict_rt", dict_rt)
+    for n in ns:
+        name_file = f"n_{n}_nbps_{len(ps_plnpca)}_p0_{pmax_pln}_pn_{pn}_ecart_{ecart}_rank_{rank}"
+        if False:
+            # if exists(name_file) is False:
+            for p in tqdm(ps_plnpca):
+                Y = df.values[:n, :p]
+                # Y, _, labels = get_sc_mark_data(max_n=n, dim=p)
+                append_running_times_model(Y, "PlnPCA", n)
+                if p < pmax_pln:
+                    append_running_times_model(Y, "Pln", n)
+                else:
+                    dict_rt["Pln"][n].append(None)
+                print("dict_rt", dict_rt)
 
-    else:
-        with open(name_file, "rb") as fp:
-            dict_rt = pickle.load(fp)
+        else:
+            with open(name_file, "rb") as fp:
+                dict_rt = pickle.load(fp)
 
-    with open(name_file, "wb") as fp:
-        pickle.dump(dict_rt, fp)
+        with open(name_file, "wb") as fp:
+            pickle.dump(dict_rt, fp)
 
-    df_results_pln = pd.DataFrame.from_dict(
-        {"Pln": dict_rt["Pln"]}, columns=ps_plnpca, orient="index"
-    )
-    df_results_plnpca = pd.DataFrame.from_dict(
-        {"PlnPCA": dict_rt["PlnPCA"]}, columns=ps_plnpca, orient="index"
-    )
+    plot_dict(dict_rt, "Pln", ns)
+    plot_dict(dict_rt, "PlnPCA", ns)
+    df_results_pln = dict_to_df("Pln")
+    print("res pln", df_results_pln)
+    df_results_plnpca = dict_to_df("PlnPCA")
+
     df_results_pln.to_csv("csv_res_benchmark/python_pln.csv")
     df_results_plnpca.to_csv("csv_res_benchmark/python_plnpca.csv")
-    plot_dict(dict_rt, "Pln")
-    plot_dict(dict_rt, "PlnPCA")
     plt.savefig(f"paper/illustration.png", format="png")
     plt.show()
--- viz.r
+++ viz.r
@@ -1,15 +1,53 @@
 library(ggplot2)
+library(grid)
+library(gridExtra)
+library(cowplot)
 
 
 file_pln = "csv_res_benchmark/python_pln.csv"
 file_plnpca = "csv_res_benchmark/python_plnpca.csv"
-
-
-plot_file <- function(filename){
+filenames = c(file_pln,file_plnpca)
+modelnames = c("Pln", "PlnPCA")
+nb_N = 2
+get_df <- function(filename, modelname){
     df = read.csv(filename, header = TRUE, check.names = F)
-    df <- data.frame(df[,-1], row.names = df[,1], check.names = F)
-    print('df')
-    print(df)
+    df <- data.frame(df[,-1], check.names = F)
+    df[,"Model"] = modelname
+    return(df)
 }
 # ?read.csv
-plot_file(file_pln)
+# plot_file(file_pln)
+
+df = get_df(filenames[1], modelnames[1])
+for (i in c(2:(length(filenames)))){
+    filename = filenames[i]
+    modelname = modelnames[i]
+    new_df = get_df(filename, modelname)
+    df = rbind(df, new_df)
+}
+df$Model = as.factor(df$Model)
+
+remove_legend <- function(myplot){
+    return(myplot + theme(legend.position="none"))
+}
+
+
+get_plot_i <- function(i){
+    current_plot <- ggplot(df) + geom_line(aes(x =dim, y = df[,colnames(df)[i]], group = Model, col = Model))
+    current_plot <- current_plot + theme_bw()+ theme(plot.title = element_text(hjust = 0.5)) + guides(fill = guide_legend(nrow = 1, byrow = TRUE))
+    current_plot <- current_plot + ggtitle(paste("n = ", colnames(df)[i])) + labs(y = "Running time (seconds)")
+}
+
+pdf("plots_benchmark.pdf")
+print('y')
+print(df)
+plots <- list()
+for (i in c(1:nb_N)){
+    tmp_plot <- get_plot_i(i)
+    common_legend = get_legend(tmp_plot)
+    tmp_plot <- remove_legend(tmp_plot)
+    plots <- append(list(tmp_plot), plots)
+}
+grid.arrange(grobs = plots, as.table = TRUE, align = c("v"), ncol = 1, bottom = common_legend, common.legend = TRUE)#, top=paste("alpha = ", alpha))
+
+dev.off()
