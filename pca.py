3445c8a1d5c4d68ccb54901494aad54ec9c63884
bastien-mva <bastien.batardiere@gmail.com>
2024-06-06 14:29:51 +0200

    last changes.

--- pca.py
+++ pca.py
@@ -7,20 +7,18 @@ import numpy as np
 import time
 
 
-# n = 1000
-# p = 15000
-n = 300
-p = 150
+n = 1000
+p = 15000
 Y, _, GT = get_sc_mark_data(max_n=n, dim=p)
 
 
 plnpca = PlnPCA(Y, rank=2)
 t = time.time()
-plnpca.fit(tol=0.001, verbose=True, nb_max_iteration=70)
+plnpca.fit(tol=0.001, verbose=True, nb_max_iteration=700)
 print("time took pln:", (time.time() - t) / 60)
 # plnpca.show()
 fig, axes = plt.subplots(1, 2, figsize=(20, 10))
-plnpca.viz(ax=axes[0])
+plnpca.viz(colors=GT, ax=axes[0])
 t = time.time()
 pca = PCA(n_components=2)
 y_pca = pca.fit_transform(np.log(Y + (Y == 0)))
@@ -37,5 +35,5 @@ axes[1].set_xlabel("PC1", fontsize=fontsize)
 axes[1].tick_params(axis="both", labelsize=fontsize - 5)
 axes[1].set_ylabel("PC2", fontsize=fontsize)
 axes[1].set_title("Standard Principal Component Analysis")
-plt.savefig("paper/figures/plnpca_vs_pca.png", format="png")
+plt.savefig("paper/figures/plnpca_vs_pca_last.png", format="png")
 plt.show()
