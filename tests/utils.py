f8d3dfa69136c1b2d04d3ac44f078b097aa64c1a
bastien-mva <bastien.batardiere@gmail.com>
2025-01-07 11:02:36 +0100

    removed all tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,22 @@
-import torch
 import functools
 
 
-def MSE(t):
-    return torch.mean(t**2)
-
-
 def filter_models(models_name):
+    """
+    Decorator to filter test functions based on the type of the first argument.
+
+    Args:
+        models_name (list): A list of model names to filter the tests.
+
+    Returns:
+        function: A decorator that wraps the test function and filters it based on the model name.
+
+    Example:
+        @filter_models(['ModelA', 'ModelB'])
+        def test_function(model):
+            # test code here
+    """
+
     def decorator(my_test):
         @functools.wraps(my_test)
         def new_test(**kwargs):
