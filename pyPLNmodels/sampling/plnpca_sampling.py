bb1077ef5890bd8d590bdc36672355eb6ff97cc7
bastien-mva <bastien.batardiere@gmail.com>
2025-01-10 17:06:42 +0100

    add PlnPCA sampling.

---
+++ pyPLNmodels/sampling/plnpca_sampling.py
@@ -0,0 +1,39 @@
+import torch
+
+
+from .pln_sampling import PlnSampler
+from ._utils import _components_from_covariance
+
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class PlnPCASampler(PlnSampler):
+    """Sampler for Poisson Log-Normal model. The parameters of the model are generated randomly."""
+
+    def __init__(
+        self,
+        n_samples: int = 100,
+        dim: int = 50,
+        rank: int = 5,
+        *,
+        nb_cov: int = 1,
+        use_offsets: bool = False,
+        marginal_mean: int = 2,
+    ):  # pylint: disable=too-many-arguments, too-many-positional-arguments
+        self._rank = rank
+        super().__init__(
+            n_samples=n_samples,
+            dim=dim,
+            nb_cov=nb_cov,
+            use_offsets=use_offsets,
+            marginal_mean=marginal_mean,
+        )
+
+    def _get_components(self):
+        return _components_from_covariance(self._params["covariance"], self._rank)
+
+    @property
+    def rank(self):
+        """Rank of the covariance matrix."""
+        return self._rank
