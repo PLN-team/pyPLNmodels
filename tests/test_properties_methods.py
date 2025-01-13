2b6ab012387669e1e38bcf9ea145ab1eb8016b24
bastien-mva <bastien.batardiere@gmail.com>
2025-01-13 18:18:26 +0100

    began to visualize a model and continue the tests.

--- tests/test_properties_methods.py
+++ tests/test_properties_methods.py
@@ -1,10 +1,15 @@
 # pylint: skip-file
+from pyPLNmodels import Pln
+
 from tests.generate_models import get_dict_models_fitted, get_model, get_fitted_model
 
 
-from pyPLNmodels import Pln
-
-
-def test_properties():
+def test_method_properties():
     pln = get_fitted_model("Pln", 2, "explicit")
-    print("methods:", pln._useful_methods_list)
+    for method, propertymethod in zip(
+        pln._useful_methods_list, pln._useful_properties_list
+    ):  # pylint: disable=protected-access
+        method = method[1:-2]
+        propertymethod = propertymethod[1:]
+        assert hasattr(pln, propertymethod)
+        assert hasattr(pln, method)
