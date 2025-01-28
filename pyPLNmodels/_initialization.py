bug here, PlnPCA not getting the right covariance nor coef.

pyPLNmodels/_initialization.py
@@ -288,31 +288,40 @@ class ZIP:
         Simple initialization of the Zero Inflated Poisson model. Coefficients are
         intialized randomly
         """
-        self.endog = endog
-        self.exog = exog
-        self.exog_inflation = exog_inflation
-        self.offsets = offsets
+        self._endog = endog.to(DEVICE)
+        if exog is not None:
+            self._exog = exog.to(DEVICE)
+        else:
+            self._exog = None
+        self._exog_inflation = exog_inflation.to(DEVICE)
+        self._offsets = offsets.to(DEVICE)
 
-        self.r0 = torch.mean((endog == 0).double(), axis=0)
-        self.ybarre = torch.mean(endog, axis=0)
+        self._r0 = torch.mean((self._endog == 0).double(), axis=0)
+        self._ybarre = torch.mean(self._endog, axis=0)
 
-        self.n_samples = endog.shape[0]
-        dim = self.endog.shape[1]
-        nb_cov = exog.shape[1]
+        self._n_samples = self._endog.shape[0]
+        dim = self._endog.shape[1]
+        nb_cov = exog.shape[1] if exog is not None else 0
         nb_cov_infla = exog_inflation.shape[1]
 
-        self.coef_inflation = (
+        self._coef_inflation = (
             torch.randn(nb_cov_infla, dim).to(DEVICE).requires_grad_(True)
         )
-        self.coef = torch.randn(nb_cov, dim).to(DEVICE).requires_grad_(True)
+        self._coef = torch.randn(nb_cov, dim).to(DEVICE).requires_grad_(True)
+
+    @property
+    def _marginal_mean(self):
+        if self._exog is None:
+            return 0
+        return self._exog @ self._coef
 
     @property
     def _mean_poisson(self):
-        return torch.exp(self.offsets + self.exog @ self.coef)
+        return torch.exp(self._offsets + self._marginal_mean)
 
     @property
     def _mean_inflation(self):
-        mean = self.exog_inflation @ self.coef_inflation
+        mean = self._exog_inflation @ self._coef_inflation
         return torch.sigmoid(mean)
 
     def loglike(self, lam, pi):
@@ -320,17 +329,27 @@ class ZIP:
         Computes the loglikelihood of a Zero Inflated Poisson regression model.
         """
         first_term = (
-            self.n_samples * self.r0 * torch.log(pi + (1 - pi) * torch.exp(-lam))
+            self._n_samples * self._r0 * torch.log(pi + (1 - pi) * torch.exp(-lam))
         )
-        second_term = self.n_samples * (1 - self.r0) * (
+        second_term = self._n_samples * (1 - self._r0) * (
             torch.log(1 - pi) - lam
-        ) + self.n_samples * self.ybarre * torch.log(lam)
+        ) + self._n_samples * self._ybarre * torch.log(lam)
         return first_term + second_term
 
     def fit(self, maxiter=150):  # pylint: disable=missing-function-docstring
-        optim = torch.optim.Rprop([self.coef, self.coef_inflation])
+        optim = torch.optim.Rprop([self._coef, self._coef_inflation])
         for _ in range(maxiter):
             loss = -torch.mean(self.loglike(self._mean_poisson, self._mean_inflation))
             loss.backward()
             optim.step()
             optim.zero_grad()
+
+    @property
+    def coef(self):
+        """Coefficient of the mean for the ZI Poisson regression model."""
+        return self._coef.cpu()
+
+    @property
+    def coef_inflation(self):
+        """Coefficient for the inflation part of the ZI Poisson regression model."""
+        return self._coef.cpu()

pyPLNmodels/elbos.py
@@ -91,17 +91,16 @@ def elbo_plnpca(  # pylint: disable=too-many-arguments
     """
     n_samples = endog.shape[0]
     rank = components.shape[1]
+    latent_variance = torch.square(latent_sqrt_variance)
+
     log_intensity = offsets + marginal_mean + latent_mean @ components.T
-    latent_var = torch.square(latent_sqrt_variance)
 
     elbo = torch.sum(endog * log_intensity)
     elbo += torch.sum(
-        -torch.exp(log_intensity + 0.5 * latent_var @ (components * components).T)
+        -torch.exp(log_intensity + 0.5 * latent_variance @ (components * components).T)
     )
-    elbo += 0.5 * torch.sum(torch.log(latent_var))
-    elbo -= 0.5 * torch.sum(
-        torch.square(latent_mean) + torch.square(latent_sqrt_variance)
-    )
+    elbo += 0.5 * torch.sum(torch.log(latent_variance))
+    elbo -= 0.5 * torch.sum(torch.square(latent_mean) + latent_variance)
     elbo -= torch.sum(_log_stirling(endog))
     elbo += 0.5 * n_samples * rank
 

pyPLNmodels/sampling/zipln_sampling.py
@@ -7,6 +7,9 @@ from .pln_sampling import PlnSampler
 from ._utils import _get_exog, _get_coef
 
 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
 class ZIPlnSampler(PlnSampler):
     """Sampler for Zero-Inflated Poisson Log-Normal model.
     The parameters of the model are generated
@@ -18,10 +21,11 @@ class ZIPlnSampler(PlnSampler):
     def __init__(
         self,
         n_samples: int = 100,
-        dim: int = 50,
+        dim: int = 20,
         *,
         nb_cov: int = 1,
         nb_cov_inflation: int = 1,
+        add_const: bool = True,
         use_offsets: bool = False,
         marginal_mean_mean: int = 2,
         marginal_mean_inflation_mean: int = 0.5,
@@ -30,13 +34,16 @@ class ZIPlnSampler(PlnSampler):
             n_samples,
             dim,
             nb_cov=nb_cov,
+            add_const=add_const,
             use_offsets=use_offsets,
             marginal_mean_mean=marginal_mean_mean,
         )
         if nb_cov_inflation == 0:
             raise ValueError("Number of covariates should should be positive.")
-        self._exog_inflation = _get_exog(n_samples, nb_cov_inflation)
-        coef_inflation = _get_coef(nb_cov_inflation, dim, marginal_mean_inflation_mean)
+        self._exog_inflation = _get_exog(n_samples, nb_cov_inflation, add_const=False)
+        coef_inflation = _get_coef(
+            nb_cov_inflation, dim, marginal_mean_inflation_mean, add_const=False
+        )
         self._params["coef_inflation"] = coef_inflation
 
     @property
@@ -46,7 +53,9 @@ class ZIPlnSampler(PlnSampler):
     @_add_doc(_BaseSampler)
     def sample(self, seed: int = 0) -> torch.Tensor:
         endog_not_inflated = super().sample()
-        self.bernoulli = torch.bernoulli(torch.sigmoid(self._marginal_mean_inflation))
+        self.bernoulli = torch.bernoulli(
+            torch.sigmoid(self._marginal_mean_inflation)
+        ).to("cpu")
         return endog_not_inflated * (1 - self.bernoulli)
 
     @property