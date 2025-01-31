6a12f2d4ab53a83efcc9450aa98eb465b22adc6c
bastien-mva <bastien.batardiere@gmail.com>
2025-01-31 11:20:51 +0100

    debug

--- tests/test_formula.py
+++ tests/test_formula.py
@@ -9,10 +9,10 @@ from pyPLNmodels import PlnPCA, ZIPln, load_microcosm
 def test_wrong_formula():
     micro = load_microcosm()
     with pytest.raises(PatsyError):
-        pca = PlnPCA.from_formula("endog ~ 1 + exog**exog", data=micro)
+        PlnPCA.from_formula("endog ~ 1 + exog**exog", data=micro)
 
 
 def test_wrong_formula_infla():
     micro = load_microcosm()
     with pytest.raises(PatsyError):
-        zi = ZIPln.from_formula("endog ~ 1 | exog**exog", data=micro)
+        ZIPln.from_formula("endog ~ 1 | exog**exog", data=micro)
