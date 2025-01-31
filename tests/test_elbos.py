76981a6b90a4e9770d2a64f92814c0252a679bbf
bastien-mva <bastien.batardiere@gmail.com>
2025-01-31 19:42:24 +0100

    Profiled zipln elbo.

--- tests/test_elbos.py
+++ tests/test_elbos.py
@@ -21,20 +21,37 @@ def test_right_pln_elbo():
     assert elbo == profiled_elbo
 
 
+def test_right_zipln_elbo():
+    data = load_scrna()
+    zi = ZIPln(data["endog"])
+    zi.fit()
+    profiled_elbo = zi.compute_elbo()
+    elbo = elbo_zipln(
+        endog=zi._endog,
+        marginal_mean=zi._marginal_mean,
+        offsets=zi._offsets,
+        latent_mean=zi._latent_mean,
+        latent_sqrt_variance=zi._latent_sqrt_variance,
+        latent_prob=zi._latent_prob,
+        covariance=zi._covariance,
+        marginal_mean_inflation=zi._marginal_mean_inflation,
+    )
+    assert elbo == profiled_elbo
+
+
 def test_error_dirac_zi():
     data = load_scrna()
     zi = ZIPln(data["endog"])
     zi.fit()
     dirac = ~zi._dirac
     with pytest.raises(RuntimeError):
-        elbo_zipln(
+        profiled_elbo_zipln(
             endog=zi._endog,
-            marginal_mean=zi._marginal_mean,
+            exog=zi._exog,
             offsets=zi._offsets,
             latent_mean=zi._latent_mean,
             latent_sqrt_variance=zi._latent_sqrt_variance,
             latent_prob=zi._latent_prob,
-            covariance=zi._covariance,
             marginal_mean_inflation=zi._marginal_mean_inflation,
             dirac=dirac,
         )
