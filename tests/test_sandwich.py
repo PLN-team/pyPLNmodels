c676a7363d468e3fcdd37a4ab2bc25f8bf763684
bastien-mva <bastien.batardiere@gmail.com>
2025-01-31 18:32:21 +0100

    Add tests for sandwich estimator.

---
+++ tests/test_sandwich.py
@@ -0,0 +1,20 @@
+# pylint: skip-file
+from .conftest import dict_fitted_models
+
+
+def test_zi():
+    for init_method in ["explicit", "formula"]:
+        for model in dict_fitted_models["Pln"][init_method]:
+            if model.nb_cov > 0:
+                model.summary()
+                coef_shape = model.coef.shape
+                assert model.get_coef_p_values().shape == coef_shape
+                interval_low, interval_high = model.get_confidence_interval_coef()
+                assert interval_low.shape == coef_shape
+                assert interval_high.shape == coef_shape
+                assert model.get_variance_coef().shape == coef_shape
+            else:
+                assert model.summary() is None
+                assert model.get_coef_p_values() is None
+                assert model.get_confidence_interval_coef() is None
+                assert model.get_variance_coef() is None
