c298f37ce0184d9f9b555ae6e7a38c1b27b4f909
bastien-mva <bastien.batardiere@gmail.com>
2025-02-05 13:45:23 +0100

    Tries network with torch implementation.

--- pyPLNmodels/network.py
+++ pyPLNmodels/network.py
@@ -11,8 +11,9 @@ from pyPLNmodels.pln import Pln
 from pyPLNmodels.elbos import elbo_pln
 from pyPLNmodels._utils import _add_doc
 from pyPLNmodels._closed_forms import _closed_formula_covariance
+from pyPLNmodels._initialization import _init_components_prec
 
-THRESHOLD = 1e-3
+THRESHOLD = 1e-5
 
 
 class PlnNetwork(Pln):
@@ -39,6 +40,9 @@ class PlnNetwork(Pln):
     """
 
     penalty: float
+    _components_prec: torch.Tensor
+    mask: torch.Tensor
+    use_gl: bool
 
     @_add_doc(
         BaseModel,
@@ -106,8 +110,13 @@ class PlnNetwork(Pln):
         tol: float = DEFAULT_TOL,
         verbose: bool = False,
         penalty=1,
+        use_gl: bool = False,
     ):  # pylint: disable = too-many-arguments
         self.penalty = penalty
+        self.use_gl = use_gl
+        self.mask = torch.ones((self.dim, self.dim)).to(self._endog.device) - torch.eye(
+            self.dim, device=self._endog.device
+        )
         super().fit(maxiter=maxiter, lr=lr, tol=tol, verbose=verbose)
 
     @_add_doc(
@@ -122,27 +131,32 @@ class PlnNetwork(Pln):
         """,
     )
     def compute_elbo(self):
+        precision = self._precision
         elbo_no_penalty = elbo_pln(
             endog=self._endog,
             marginal_mean=self._marginal_mean,
             offsets=self._offsets,
             latent_mean=self._latent_mean,
             latent_sqrt_variance=self._latent_sqrt_variance,
-            precision=self._precision,
+            precision=precision,
         )
-        return elbo_no_penalty
+        return elbo_no_penalty - self.penalty * self._l0_penalty(precision)
+
+    def _l0_penalty(self, precision):
+        return torch.norm(precision * self.mask, p=1)
 
     @property
     def _precision(self):
-        covariance = _closed_formula_covariance(
-            self._marginal_mean,
-            self._latent_mean,
-            self._latent_sqrt_variance,
-            self.n_samples,
-        )
-        _, precision = GL(covariance.detach().numpy(), alpha=self.penalty)
-        precision = torch.from_numpy(precision)
-        return precision
+        if self.use_gl is True:
+            covariance = _closed_formula_covariance(
+                self._marginal_mean,
+                self._latent_mean,
+                self._latent_sqrt_variance,
+                self.n_samples,
+            )
+            _, precision = GL(covariance.detach().cpu().numpy(), alpha=self.penalty)
+            return torch.from_numpy(precision).to(self._endog.device)
+        return self._components_prec @ (self._components_prec.T)
 
     @property
     def precision(self):
@@ -166,3 +180,14 @@ class PlnNetwork(Pln):
     @property
     def _nb_zeros_precision(self):
         return torch.sum((torch.abs(self.precision) < THRESHOLD).float()) / 2
+
+    def _init_model_parameters(self):
+        if self.use_gl is False:
+            self._components_prec = _init_components_prec(self._endog)
+
+    @property
+    def list_of_parameters_needing_gradient(self):
+        list_params = [self._latent_mean, self._latent_sqrt_variance]
+        if self.use_gl is False:
+            list_params.append(self._components_prec)
+        return list_params
