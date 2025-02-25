312f8c4a7c6f546ccb3ac1a658bc5656892abe01
bastien-mva <bastien.batardiere@gmail.com>
2025-02-25 17:26:08 +0100

    Takes the tests.

--- tests/test_convergence.py
+++ tests/test_convergence.py
@@ -6,17 +6,16 @@ from .conftest import dict_fitted_models
 
 
 def test_median():
-    i = 0
     for model_name in dict_fitted_models.keys():
         for init_method in ["formula", "explicit"]:
             for model in dict_fitted_models[model_name][init_method]:
                 if model_name == "PlnMixture":
                     mapping = _get_argmax_mapping(model.cluster_bias)
-                i += 1
                 for param_key, param in model.dict_model_parameters.items():
                     if param is not None:
                         if param_key in ["covariances", "cluster_bias"]:
                             param = param[mapping]
+                        print("model_name", model_name)
                         err = median(
                             param - model.sampler.dict_model_true_parameters[param_key]
                         )
