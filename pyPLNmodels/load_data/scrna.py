3e3d87be2d7bec3475531a53fb65a6561375128b
bastien-mva <bastien.batardiere@gmail.com>
2025-03-02 18:34:34 +0100

    Change  the size of scrna dataset.

--- pyPLNmodels/load_data/scrna.py
+++ pyPLNmodels/load_data/scrna.py
@@ -20,18 +20,18 @@ def load_scrna(
     Parameters
     ----------
     n_samples : int, optional
-        Number of samples, by default 400. Cannot be greater than 400.
+        Number of samples, by default 400. Cannot be greater than 2000.
     dim : int, optional
         Dimension (i.e. number of variables, genes), by default 100.
-        Cannot be greater than 100.
+        Cannot be greater than 3000.
 
     Returns
     -------
     Dict
         Dictionary with three keys: `endog`, `labels`, and `labels_1hot`.
             `endog` is a matrix of counts.
-            `labels` is an array giving cell types with two possibilities,
-                    either `T_cells_CD4+` or `T_cells_CD8+`.
+            `labels` is an array giving cell types with three possibilities,
+                    either `T_cells_CD4+` or `T_cells_CD8+` or `Macrophages`.
             `labels_1hot` corresponds to a 2D array that is the
                 one-hot encoding of the `labels` array.
 
@@ -43,8 +43,8 @@ def load_scrna(
     >>> print(scrna["endog"].head())
     >>> print(scrna["endog"].describe())
     """
-    max_samples = 400
-    max_dim = 100
+    max_samples = 2000
+    max_dim = 3000
     n_samples, dim = _threshold_samples_and_dim(max_samples, max_dim, n_samples, dim)
     endog = pd.read_csv(
         pkg_resources.resource_stream(__name__, "data/scRT/counts.csv")
