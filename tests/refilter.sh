2ba8cec4fcf9a1961326bb54d2a5b637dd24f940
bastien-mva <bastien.batardiere@gmail.com>
2025-03-18 15:43:03 +0100

    Continue

--- tests/refilter.sh
+++ tests/refilter.sh
@@ -6,4 +6,19 @@ python _create_readme_getting_started_and_docstrings_tests.py
 
 cd docstrings_examples/models/
 
-./run.sh
+output_file="output.txt"
+
+# Clear the output file if it exists
+> "$output_file"
+
+# Traverse directories and subdirectories
+find . -name "*.py" | while read -r file; do
+    # Run the Python file
+    if ! python3 "$file" 2>> "$output_file"; then
+        # If there is an error, log it to output.txt
+        echo "Error in $file" >> "$output_file"
+    else
+        # If no error, remove the file
+        rm "$file"
+    fi
+done
