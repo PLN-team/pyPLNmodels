4ea16332f28eed91deb4e9799a82dc8ed5887e7e
bastien-mva <bastien.batardiere@gmail.com>
2025-03-26 12:44:06 +0100

    tests all the current tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from .generate_models import (
     get_dict_collections_fitted,
 )
 
-# dict_unfit_models = get_dict_models_unfit()
-# dict_fitted_models = get_dict_models_fitted()
-# dict_collections_fitted = get_dict_collections_fitted()
+dict_unfit_models = get_dict_models_unfit()
+dict_fitted_models = get_dict_models_fitted()
+dict_collections_fitted = get_dict_collections_fitted()
